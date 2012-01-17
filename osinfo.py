2c811e89f1912c639dc7695a1abecab975595bab
Craig Richards <craig@geekcomputers.co.uk>
2012-01-17 08:33:30 +0000

    Very simple way to gather some OS information

---
+++ osinfo.py
@@ -0,0 +1,21 @@
+import platform
+
+profile = [
+platform.architecture(),
+platform.dist(),
+platform.libc_ver(),
+platform.mac_ver(),
+platform.machine(),
+platform.node(),
+platform.platform(),
+platform.processor(),
+platform.python_build(),
+platform.python_compiler(),
+platform.python_version(),
+platform.system(),
+platform.uname(),
+platform.version(),
+]
+
+for item in profile:
+  print item
