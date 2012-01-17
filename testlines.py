8f19bb228042b7b0019b1087f0add66a06c388ea
Craig Richards <craig@geekcomputers.co.uk>
2012-01-17 08:35:37 +0000

    This is a simple script that will print out x number of lines of text to a file, handy when you get lines at school !!

---
+++ testlines.py
@@ -0,0 +1,15 @@
+# Script Name		: testlines.py
+# Author				: Craig Richards
+# Created				: 08th December 2011
+# Last Modified		: 
+# Version				: 1.0
+
+# Modifications		: 
+
+# Description			: This very simple script open a file and prints out 100 lines of whatever is set for the line variable
+
+line="Test you want to print\n"	# This sets the variable for the text that you want to print
+f=open('mylines.txt','w')				# Create the file to store the output
+for i in range(1,101):					# Loop 100 times
+  f.write(line)								# Write the text to the file
+f.close()									# Close the file
