5824a1c7920aff2754a38e2114133dd43ac17d01
Craig Richards <craig@geekcomputers.co.uk>
2012-06-12 23:12:37 +0100

    This very simple script opens the file server_list.txt and the does an nslookup for each one to check the DNS entry

--- nslookup_check.py
+++ nslookup_check.py
@@ -8,6 +8,6 @@
 
 # Description			: This very simple script opens the file server_list.txt and the does an nslookup for each one to check the DNS entry
 
-import subprocess								# Import the subprocess module
-for server in open('server_list.txt'):		# Open the file and read each line
+import subprocess										# Import the subprocess module
+for server in open('server_list.txt'):				# Open the file and read each line
 	subprocess.Popen(('nslookup '+server))	# Run the nslookup command for each server in the list
