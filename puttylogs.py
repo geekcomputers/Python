b4b5430caa8e40dc1cc1ff0fe7f814a271c20c9c
Craig Richards <craig@geekcomputers.co.uk>
2012-06-13 22:32:34 +0100

    Zip up all the logs in the given directory

--- puttylogs.py
+++ puttylogs.py
@@ -22,6 +22,6 @@ for files in os.listdir(logsdir):										# Find all the files in the directory
 		files1=files+"."+strftime("%Y-%m-%d")+".zip"		# Create the Variable files1, this is the files in the directory, then we add a suffix with the date and the zip extension
 		os.chdir(logsdir) 												# Change directory to the logsdir
 		os.system(zip_program + " " +  files1 +" "+ files)	# Zip the logs into dated zip files for each server. - 1.1
-		shutil.move(files1, zipdir)									# Move the zipped log files to the zipped_logs directort - 1.2
+		shutil.move(files1, zipdir)									# Move the zipped log files to the zipped_logs directory - 1.2
 		os.remove(files)													# Remove the original log files
 		
