1ab51598b41a3edbeeba902b1186d285fbe8a1e6
Craig Richards <craig@geekcomputers.co.uk>
2012-06-13 22:05:59 +0100

    This very simple script open a file and prints out 100 lines of whatever is set for the line variable

--- testlines.py
+++ testlines.py
@@ -13,3 +13,4 @@ f=open('mylines.txt','w')				# Create the file to store the output
 for i in range(1,101):					# Loop 100 times
   f.write(line)								# Write the text to the file
 f.close()									# Close the file
+
