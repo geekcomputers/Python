9619c868c6422f42e9a40a48f92768f629db226a
Craig Richards <craig@geekcomputers.co.uk>
2012-06-14 08:14:38 +0100

    This will list all the files in the given directory, it will also go through all the subdirectories as well

--- script_listing.py
+++ script_listing.py
@@ -1,9 +1,11 @@
 # Script Name		: script_listing.py
 # Author				: Craig Richards
 # Created				: 15th February 2012
-# Last Modified		: 
-# Version				: 1.1
+# Last Modified		: 29th May 2012
+# Version				: 1.2
+
 # Modifications		: 1.1 - 28-02-2012 - CR - Added the variable to get the logs directory, I then joined the output so the file goes to the logs directory
+#							: 1.2 - 29-05/2012 - CR - Changed the line so it doesn't ask for a directory, it now uses the environment varaible scripts
 
 # Description			: This will list all the files in the given directory, it will also go through all the subdirectories as well
 
@@ -11,8 +13,9 @@ import os																		# Load the library module
 
 logdir = os.getenv("logs")												# Set the variable logdir by getting the value from the OS environment variable logs
 logfile = 'script_list.log'													# Set the variable logfile
+path = os.getenv("scripts")												# Set the varable path by getting the value from the OS environment variable scripts - 1.2
 
-path = (raw_input("Enter dir: "))										# Ask the user for the directory to scan
+#path = (raw_input("Enter dir: "))										# Ask the user for the directory to scan
 logfilename=os.path.join(logdir, logfile)								# Set the variable logfilename by joining logdir and logfile together
 log = open(logfilename, 'w')												# Set the variable log and open the logfile for writing
 for dirpath, dirname, filenames in os.walk(path):				# Go through the directories and the subdirectories
