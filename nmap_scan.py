e62f91ba78c18bad55dd9f96dbee45a1e398f09d
geekcomputers <craig@geekcomputers.co.uk>
2013-06-14 08:31:30 +0100

    Carry out a nmap scan in python
    Need to download the nmap module and have nmap instaled on the system

---
+++ nmap_scan.py
@@ -0,0 +1,36 @@
+# Script Name		: nmap_scan.py
+# Author				: Craig Richards
+# Created				: 24th May 2013
+# Last Modified		: 
+# Version				: 1.0
+
+# Modifications		: 
+
+# Description			: This scans my scripts directory and gives a count of the different types of scripts, you need nmap installed to run this
+
+import nmap			# Import the module
+import optparse		# Import the module
+
+def nmapScan(tgtHost, tgtPort):									# Create the function, this fucntion does the scanning
+  nmScan = nmap.PortScanner()
+  nmScan.scan(tgtHost, tgtPort)
+  state = nmScan[tgtHost]['tcp'][int(tgtPort)]['state']
+  print "[*] " + tgtHost + " tcp/"+tgtPort +" "+state
+  
+def main():																								# Main Program
+  parser = optparse.OptionParser('usage%prog ' + '-H <host> -p <port>')		# Display options/help if required
+  parser.add_option('-H', dest='tgtHost', type='string', help='specify host')
+  parser.add_option('-p', dest='tgtPort', type='string', help='port')
+  (options, args) = parser.parse_args()
+  tgtHost = options.tgtHost
+  tgtPorts = str(options.tgtPort).split(',')
+  
+  if (tgtHost == None) | (tgtPorts[0] == None):  
+    print parser.usage
+    exit(0)
+
+  for tgtPort in tgtPorts:																			# Scan the hosts with the ports etc
+    nmapScan(tgtHost, tgtPort)
+
+if __name__ == '__main__':
+  main()
