dca30f890ccde4e65a2a7f77bf34d970f7f71f54
geekcomputers <craig@geekcomputers.co.uk>
2013-06-17 21:38:16 +0100

    Changed to functions, and check os before running the program

--- pscheck.py
+++ pscheck.py
@@ -1,18 +1,19 @@
 # Script Name		: pscheck.py
 # Author				: Craig Richards
 # Created				: 19th December 2011
-# Last Modified		: 
-# Version				: 1.0
+# Last Modified		: 17th June 2013
+# Version				: 1.1
 
-# Modifications		: 
+# Modifications		: 1.1 - 17/06/13 - CR - Changed to functions, and check os before running the program
 
 # Description			: Process check on Nix boxes, diplsay formatted output from ps command
 
 import commands, os, string
 
-program = raw_input("Enter the name of the program to check: ")
+def ps():
+  program = raw_input("Enter the name of the program to check: ")
 
-try:
+  try:
     #perform a ps command and assign results to a list
     output = commands.getoutput("ps -f|grep " + program)
     proginfo = string.split(output)
@@ -24,7 +25,15 @@ try:
     Process ID:\t\t", proginfo[1], "\n\
     Parent process ID:\t", proginfo[2], "\n\
     Time started:\t\t", proginfo[4]
-except:
+  except:
     print "There was a problem with the program." 
 
-
+def main():
+  if os.name == "posix":											# Unix/Linux/MacOS/BSD/etc
+    ps()																	# Call the function
+  elif os.name in ("nt", "dos", "ce"):							# if the OS is windows
+    print "You need to be on Linux or Unix to run this"
+		 
+		 
+if __name__ == '__main__':
+  main()
