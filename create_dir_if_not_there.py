12967eb9e4a5f5653eed2c827ea272f4eb8a4358
Harsha Kuchampudi <harshakuchampudi@gmail.com>
2014-12-27 21:22:14 -0500

    PEP8 Fixes

--- create_dir_if_not_there.py
+++ create_dir_if_not_there.py
@@ -1,14 +1,14 @@
-# Script Name		: create_dir_if_not_there.py
-# Author				: Craig Richards
-# Created				: 09th January 2012
-# Last Modified		: 
-# Version				: 1.0
-# Modifications		: 
+# Script Name	: create_dir_if_not_there.py
+# Author		: Craig Richards
+# Created		: 09th January 2012
+# Last Modified	:
+# Version		: 1.0
+# Modifications	:
+# Description	: Checks to see if a directory exists in the users home directory, if not then create it
 
-# Description			: Checks to see if a directory exists in the users home directory, if not then create it
+import os  # Import the OS module
 
-import os												# Import the OS module
-home=os.path.expanduser("~")				# Set the variable home by expanding the users set home directory
-print home												# Print the location
-if not os.path.exists(home+'/testdir'):		# Check to see if the directory exists
-  os.makedirs(home+'/testdir')					# If not create the directory, inside their home directory
+home = os.path.expanduser("~")  # Set the variable home by expanding the users set home directory
+print home  # Print the location
+if not os.path.exists(home + '/testdir'):  # Check to see if the directory exists
+    os.makedirs(home + '/testdir')  # If not create the directory, inside their home directory
