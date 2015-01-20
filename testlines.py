1e863681090a96ca31669e329712702242c20d8e
bevennyamande <bevennyamande@gmail.com>
2015-01-20 13:05:14 +0200

    Update testlines.py

--- testlines.py
+++ testlines.py
@@ -1,12 +1,18 @@
 # Script Name		: testlines.py
 # Author				: Craig Richards
 # Created				: 08th December 2011
-# Last Modified		: 
+# Last Modified		: 20 January 2015 by Beven Nyamande
 # Version				: 1.0
 
 # Modifications		: 
 
 # Description			: This very simple script open a file and prints out 100 lines of whatever is set for the line variable
+
+
+# thinking of printing a heart to a file 
+# will do that
+
+
 
 line="Test you want to print\n"	# This sets the variable for the text that you want to print
 f=open('mylines.txt','w')				# Create the file to store the output
