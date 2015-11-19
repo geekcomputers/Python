0140659853f9749f21c4ae9f92bd1165495bcf73
Ricky Wilson <echoquote@gmail.com>
2015-11-19 16:37:42 -0600

    modified:   batch_file_rename.py
    I made batch_file_rename.py PEP8 complient.

--- batch_file_rename.py
+++ batch_file_rename.py
@@ -1,25 +1,51 @@
-# Script Name		: batch_file_rename.py
-# Author				: Craig Richards
-# Created				: 6th August 2012
-# Last Modified		: 
-# Version				: 1.0
+# batch_file_rename.py
+# Created: 6th August 2012
 
-# Modifications		: 
+'''
+This will batch rename a group of files in a given directory,
+once you pass the current and new extensions
+'''
 
-# Description			: This will batch rename a group of files in a given directory, once you pass the current and new extensions
+__author__ = 'Craig Richards'
+__version__ = '1.0'
 
-import os															# Load the library module
-import sys															# Load the library module
+import os
+import sys
 
-work_dir=sys.argv[1]											# Set the variable work_dir with the first argument passed
-old_ext=sys.argv[2]											# Set the variable work_dir with the first argument passed
-new_ext=sys.argv[3]											# Set the variable work_dir with the first argument passed
 
-files = os.listdir(work_dir)									# Set the variable files, by listing everything in the directory 
-for filename in files:											# Loop through the files
-  file_ext = os.path.splitext(filename)[1]				# Get the file extension
-  if old_ext == file_ext:										# Start of the logic to check the file extensions, if old_ext = file_ext
-    newfile = filename.replace(old_ext, new_ext)	# Set newfile to be the filename, replaced with the new extension
-    os.rename(													# Write the files
-	    os.path.join(work_dir, filename),
-		os.path.join(work_dir, newfile))
+def batch_rename(work_dir, old_ext, new_ext):
+    '''
+    This will batch rename a group of files in a given directory,
+    once you pass the current and new extensions
+    '''
+    # files = os.listdir(work_dir)
+    for filename in os.listdir(work_dir):
+        # Get the file extension
+        file_ext = os.path.splitext(filename)[1]
+        # Start of the logic to check the file extensions, if old_ext = file_ext
+        if old_ext == file_ext:
+            # Set newfile to be the filename, replaced with the new extension
+            newfile = filename.replace(old_ext, new_ext)
+            # Write the files
+            os.rename(
+              os.path.join(work_dir, filename),
+              os.path.join(work_dir, newfile)
+            )
+
+
+def main():
+    '''
+    This will be called if the script is directly envoked.
+    '''
+    # Set the variable work_dir with the first argument passed
+    work_dir = sys.argv[1]
+    # Set the variable old_ext with the second argument passed
+    old_ext = sys.argv[2]
+    # Set the variable new_ext with the third argument passed
+    new_ext = sys.argv[3]
+    batch_rename(work_dir, old_ext, new_ext)
+
+
+if __name__ == '__main__':
+    main()
+
