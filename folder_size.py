ebaad918295360b14fc838ad1c64f38a20edfe68
Jared Manning <jaredmanning@users.noreply.github.com>
2016-02-14 02:27:33 -0600

    REVERT 83432b9
    I'm not sure how to revert this other than like this. My apologies for commit that crap.

--- folder_size.py
+++ folder_size.py
@@ -13,15 +13,11 @@ import os       # Load the library module
 directory = '.' # Set the variable directory to be the current directory
 dir_size = 0    # Set the size to 0
 
-def main():
-    for (path, dirs, files) in os.walk(directory):      # Walk through all the directories
-        for file in files:                              # Get all the files
-            filename = os.path.join(path, file)
-            dir_size += os.path.getsize(filename)       # Get the sizes, the following lines print the sizes in bytes, Kb, Mb and Gb
-    print "Folder Size in Bytes = %0.2f Bytes" % (dir_size)
-    print "Folder Size in Kilobytes = %0.2f KB" % (dir_size/1024.0)
-    print "Folder Size in Megabytes = %0.2f MB" % (dir_size/1024/1024.0)
-    print "Folder Size in Gigabytes = %0.2f GB" % (dir_size/1024/1024/1024.0)
-
-if __name__ == '__main__':
-    main()
+for (path, dirs, files) in os.walk(directory):      # Walk through all the directories
+    for file in files:                              # Get all the files
+        filename = os.path.join(path, file)
+        dir_size += os.path.getsize(filename)       # Get the sizes, the following lines print the sizes in bytes, Kb, Mb and Gb
+print "Folder Size in Bytes = %0.2f Bytes" % (dir_size)
+print "Folder Size in Kilobytes = %0.2f KB" % (dir_size/1024.0)
+print "Folder Size in Megabytes = %0.2f MB" % (dir_size/1024/1024.0)
+print "Folder Size in Gigabytes = %0.2f GB" % (dir_size/1024/1024/1024.0)
