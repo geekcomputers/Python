29f7638181999e67f7f6ec2a9247c9f58ef9ce75
Rutvik Narayana Nadimpally <rutvik.narayana@gmail.com>
2016-02-22 11:51:20 -0500

    Update folder_size.py
    Made the code more compact by iterating through a dictionary. The line the dictionary and print the file size in Bytes, Kilobytes, Megabytes and Gigabytes.

--- folder_size.py
+++ folder_size.py
@@ -1,23 +1,25 @@
 # Script Name   : folder_size.py
 # Author        : Craig Richards
 # Created       : 19th July 2012
-# Last Modified	: 14 February 2016
+# Last Modified	: 22 February 2016
 # Version       : 1.0.1
 
-# Modifications : 1.0.1 - Tidy up comments and syntax, add main() function
+# Modifications : Modified the Printing method and added a few comments
 
 # Description   : This will scan the current directory and all subdirectories and display the size.
 
 import os       # Load the library module
-
 directory = '.' # Set the variable directory to be the current directory
 dir_size = 0    # Set the size to 0
 
-for (path, dirs, files) in os.walk(directory):      # Walk through all the directories
+fsizedicr = {'Bytes': 1, 'Kilobytes': float(1)/1024, 'Megabytes': float(1)/(1024*1024), 'Gigabytes': float(1)/(1024*1024
+                                                                                                               *
+                                                                                                               1024)}
+                                                                                                               
+for (path, dirs, files) in os.walk(directory):      # Walk through all the directories. For each iteration, os.walk returns the folders, subfolders and files in the dir.
     for file in files:                              # Get all the files
-        filename = os.path.join(path, file)
-        dir_size += os.path.getsize(filename)       # Get the sizes, the following lines print the sizes in bytes, Kb, Mb and Gb
-print "Folder Size in Bytes = %0.2f Bytes" % (dir_size)
-print "Folder Size in Kilobytes = %0.2f KB" % (dir_size/1024.0)
-print "Folder Size in Megabytes = %0.2f MB" % (dir_size/1024/1024.0)
-print "Folder Size in Gigabytes = %0.2f GB" % (dir_size/1024/1024/1024.0)
+        filename = os.path.join(path, file)         
+        dir_size += os.path.getsize(filename)       # Add the size of each file in the root dir to get the total size.
+        
+for key in fsizedicr:       #iterating through the dictionary
+    print ("Folder Size: " + str(round(fsizedicr[key]*dir_size, 2)) + " " + key)        # round function example: round(4.2384, 2) ==> 4.23
