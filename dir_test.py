17d6a11adbb2ef42e728c076714f86b95ff472c9
kKATO_lab <di.aps53@gmail.com>
2016-05-22 20:44:01 +0900

    convert indent to space / reformat files based on PEP8

--- dir_test.py
+++ dir_test.py
@@ -7,6 +7,7 @@
 
 # Description			: Tests to see if the directory testdir exists, if not it will create the directory for you
 
-import os									# Import the OS module
-if not os.path.exists('testdir'):		#  Check to see if it exists  
-  os.makedirs('testdir')				#  Create the directory 
+import os  # Import the OS module
+
+if not os.path.exists('testdir'):  # Check to see if it exists
+    os.makedirs('testdir')  # Create the directory
--- ping_subnet.py
+++ ping_subnet.py
@@ -20,20 +20,20 @@ You need to supply the first octets of the address Usage : ''' + filename + ''' 
     sys.exit(0)
 else:
 
-	if (len(sys.argv) < 2): 																				# If no arguments are passed then display the help and instructions on how to run the script
-		sys.exit (' You need to supply the first octets of the address Usage : ' + filename + ' 111.111.111')
+    if (len(sys.argv) < 2): 																				# If no arguments are passed then display the help and instructions on how to run the script
+        sys.exit (' You need to supply the first octets of the address Usage : ' + filename + ' 111.111.111')
 
-	subnet = sys.argv[1]																				# Set the variable subnet as the three octets you pass it
-	
-	if os.name == "posix":																			# Check the os, if it's linux then			
-		myping = "ping -c 2 "																			# This is the ping command
-	elif os.name in ("nt", "dos", "ce"):															# Check the os, if it's windows then
-		myping = "ping -n 2 "																			# This is the ping command
+    subnet = sys.argv[1]																				# Set the variable subnet as the three octets you pass it
 
-	f = open('ping_'+subnet+'.log', 'w')															# Open a logfile
-	for ip in range(2,255):																				# Set the ip variable for the range of numbers
-		ret = subprocess.call(myping + str(subnet)+"."+str(ip) , shell=True,stdout=f,stderr=subprocess.STDOUT) # Run the command pinging the servers
-		if ret == 0:																							# Depending on the response
-			f.write (subnet+"."+str(ip) + " is alive" + "\n")									# Write out that you can receive a reponse
-		else: 
-			f.write (subnet+"."+str(ip) + " did not respond" + "\n")						# Write out you can't reach the box
+    if os.name == "posix":																			# Check the os, if it's linux then
+        myping = "ping -c 2 "																			# This is the ping command
+    elif os.name in ("nt", "dos", "ce"):															# Check the os, if it's windows then
+        myping = "ping -n 2 "																			# This is the ping command
+
+    f = open('ping_'+subnet+'.log', 'w')															# Open a logfile
+    for ip in range(2,255):																				# Set the ip variable for the range of numbers
+        ret = subprocess.call(myping + str(subnet)+"."+str(ip) , shell=True,stdout=f,stderr=subprocess.STDOUT) # Run the command pinging the servers
+        if ret == 0:																							# Depending on the response
+            f.write (subnet+"."+str(ip) + " is alive" + "\n")									# Write out that you can receive a reponse
+        else:
+            f.write (subnet+"."+str(ip) + " did not respond" + "\n")						# Write out you can't reach the box
