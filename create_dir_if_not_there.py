1d4fe39bf9453afe3737878a8b9b24ed9265a38c
rachit.ranjan93 <rachit.ranjan93@gmail.com>
2016-06-30 00:55:58 +0530

    Added Exception Handling and User Inputs

--- create_dir_if_not_there.py
+++ create_dir_if_not_there.py
@@ -1,17 +1,27 @@
 # Script Name		: create_dir_if_not_there.py
-# Author				: Craig Richards
-# Created				: 09th January 2012
-# Last Modified		: 22nd October 2015
-# Version				: 1.0
+# Author		: Craig Richards
+# Created		: 09th January 2012
+# Last Modified		: 29th July 2016
+# Version		: 1.1
 # Modifications		: Added exceptions
+#                        1.0.1 Tidy up comments and syntax
+#                       : 1.1 Added User Input for Directory Name
+#               	:     Added Messages with OS specific separators
+#               	:     Tidied up the Indentation
+# Description		: Checks to see if a directory exists in the users home directory, if not then create it
 
-# Description			: Checks to see if a directory exists in the users home directory, if not then create it
+import os		# Import the OS module
+dir = raw_input("Enter the directory name to be created\n")
+dir = os.sep + dir
 
-import os			# Import the OS module
 try:
-  home=os.path.expanduser("~")				# Set the variable home by expanding the users set home directory
-  print home												# Print the location
-  if not os.path.exists(home+'/testdir'):
-    os.makedirs(home+'/testdir')  	# If not create the directory, inside their home directory
-  except Exceptions as e:
-    print e
+    home = os.path.expanduser("~")          # Set the variable home by expanding the users set home directory
+    print "Home: %s" % home                              # Print the location
+
+    if not os.path.exists(home+dir):
+        os.makedirs(home+dir)        # If not create the directory, inside their home directory
+        print "Created %s" % home+dir
+    else:
+        print "Directory Already Present"
+except OSError as e:
+        print "Exception: %r" % e
--- dir_test.py
+++ dir_test.py
@@ -1,12 +1,25 @@
 # Script Name		: dir_test.py
-# Author				: Craig Richards
-# Created				: 29th November 2011
-# Last Modified		:
-# Version				: 1.0
-# Modifications		: 
+# Author		: Craig Richards
+# Created		: 29th November 2011
+# Last Modified		: 29th July 2016
+# Version		: 1.1
+# Modifications		: 1.1 Added User Input for Directory Name along with else case
+#                       :     Added Messages and Basic Exception Handling
 
 # Description			: Tests to see if the directory testdir exists, if not it will create the directory for you
 
-import os									# Import the OS module
-if not os.path.exists('testdir'):		#  Check to see if it exists  
-  os.makedirs('testdir')				#  Create the directory 
+import os  # Import the OS module
+
+
+print "Current Path: %s" % os.getcwd()
+dir = raw_input("Enter Directory Name to look for in current path\n")
+
+os_sep_dir = os.getcwd() + os.sep + dir
+try:
+    if not os.path.exists(dir):  # Check to see if it exists
+        os.makedirs(dir)  # Create the directory
+        print "%s created" % (os_sep_dir)
+    else:
+        print "%s already exists" % (os_sep_dir)
+except OSError as e:
+    print "Exception: %r" % e
