5a9dc870fb4cd6fc6983689c2221c6bf44a584ab
Harshil <darjiharshil2994@gmail.com>
2016-07-22 00:04:43 +0530

    Update get_youtube_view.py

--- get_youtube_view.py
+++ get_youtube_view.py
@@ -4,6 +4,6 @@ import webbrowser
 #how much views you want
 
 print("Enjoy your Time\n" +time.ctime())
-for count in range(2):
+for count in range(30):
     time.sleep(5) 
     webbrowser.open("https://www.youtube.com/watch?v=o6A7nf3IeeA")
