9cda0edf1447f3ad5f4f1d106373059af92a9fae
William Monroe <williebmonroe@gmail.com>
2016-09-19 00:22:44 -0700

    Update to folder_size.py
    Noticed that this file prints file size without regard to the magnitude of the units. This edit changes the original print function into a list comprehension and reverse-sorts it to print the file size in ascending order of magnitude.

--- folder_size.py
+++ folder_size.py
@@ -21,5 +21,9 @@ for (path, dirs, files) in os.walk(directory):      # Walk through all the direc
         filename = os.path.join(path, file)         
         dir_size += os.path.getsize(filename)       # Add the size of each file in the root dir to get the total size.
         
-for key in fsizedicr:       #iterating through the dictionary
-    print ("Folder Size: " + str(round(fsizedicr[key]*dir_size, 2)) + " " + key)        # round function example: round(4.2384, 2) ==> 4.23
+fsizeList = [str(round(fsizedicr[key]*dir_size, 2)) + " " + key for key in fsizedicr] # List of units 
+
+if dir_size == 0: print ("File Empty") # Sanity check to eliminate corner-case of empty file. 
+else:
+  for units in sorted(fsizeList)[::-1]: # Reverse sort list of units so smallest magnitude units print first.
+      print ("Folder Size: " + units)
