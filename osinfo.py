4542bc439694ca3c1513cf84d9ab1d9e6f6dfa82
Vishvajit Pathak <vishvajit.pathak@gmail.com>
2016-11-15 16:31:02 +0530

    PEP8 format

--- osinfo.py
+++ osinfo.py
@@ -5,8 +5,8 @@
 # Version		: 1.0
 
 # Modification 1	: Changed the profile to list again. Order is important. Everytime we run script we don't want to see different ordering.
-# Modification 2        : Fixed the AttributeError checking for all properties. Using hasttr(). 
-# Modification 3        : Removed ': ' from properties inside profile.  
+# Modification 2        : Fixed the AttributeError checking for all properties. Using hasttr().
+# Modification 3        : Removed ': ' from properties inside profile.
 
 
 # Description		: Displays some information about the OS you are running this script on
@@ -14,21 +14,22 @@
 import platform as pl
 
 profile = [
-'architecture',
-'linux_distribution',
-'mac_ver',
-'machine',
-'node',
-'platform',
-'processor',
-'python_build',
-'python_compiler',
-'python_version',
-'release',
-'system',
-'uname',
-'version',
-]
+        'architecture',
+        'linux_distribution',
+        'mac_ver',
+        'machine',
+        'node',
+        'platform',
+        'processor',
+        'python_build',
+        'python_compiler',
+        'python_version',
+        'release',
+        'system',
+        'uname',
+        'version',
+    ]
+
 
 class bcolors:
     HEADER = '\033[95m'
@@ -41,8 +42,7 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
-
 for key in profile:
-    if hasattr(pl,key):
-        print(key + bcolors.BOLD + ": "+ str(getattr(pl,key)())+ bcolors.ENDC)
-        
+    if hasattr(pl, key):
+        print(key + bcolors.BOLD + ": " + str(getattr(pl, key)()) + bcolors.ENDC)
+
