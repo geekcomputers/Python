ba454d4f28237a77f0bccfe30f94e816b694e912
Jarosław <jar0m@yandex.com>
2017-03-13 22:49:42 +0000

    Update create_dir_if_not_there.py

--- create_dir_if_not_there.py
+++ create_dir_if_not_there.py
@@ -13,7 +13,7 @@ try:
     home = os.path.expanduser("~")          # Set the variable home by expanding the users set home directory
     print(home)                              # Print the location
 
-    if not os.path.exists(os.path.join(home, 'testdir0123')):
-        os.makedirs(os.path.join(home, 'testdir0123'))        # If not create the directory, inside their home directory
+    if not os.path.exists(os.path.join(home, 'testdir')):
+        os.makedirs(os.path.join(home, 'testdir'))        # If not create the directory, inside their home directory
 except Exception as e:
     print(e)
