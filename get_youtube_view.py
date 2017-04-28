e0ce06f058e6950b99ac91271d4ad7786a7d941f
Barnaby Sandeford <barnabysandeford@gmail.com>
2017-04-28 15:27:49 +0100

    Adjusted for more efficiency and functionality
    Now refreshes the page instead of creating new tabs

--- get_youtube_view.py
+++ get_youtube_view.py
@@ -1,10 +1,23 @@
-import time
-import webbrowser
+"""
+Created on Thu Apr 27 16:28:36 2017
+@author: barnabysandeford
+"""
+# Currently works for Safari, but just change to whichever 
+# browser you're using
 
-#how much views you want
-#This only works when video has less than 300 views, it won't work when there are more than 300 views...
-#due to youtube's policy.
-print("Enjoy your Time\n" + time.ctime())
-for count in range(30):
-    time.sleep(5)
-    webbrowser.open("https://www.youtube.com/watch?v=o6A7nf3IeeA")
+import time
+from selenium import webdriver
+
+count = int(raw_input("Number of times to be repeated: "))
+x = raw_input("Enter the URL (no https): ")
+print( "Length of video:")
+minutes = int(raw_input("Minutes "))
+seconds  = int(raw_input("Seconds "))
+
+refreshrate = minutes * 60 + seconds
+driver = webdriver.Safari()
+driver.get("http://"+x)
+
+for i in range(count):
+    time.sleep(refreshrate)
+    driver.refresh()
