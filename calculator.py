d81eb50aeb147a8018488d587b0a3763d276bcb6
shreydan <shreyasdaniel2001@yahoo.co.in>
2017-05-12 21:01:43 +0530

    updated calculator.py

--- calculator.py
+++ calculator.py
@@ -1,40 +1,58 @@
 """
 Written by: Shreyas Daniel - github.com/shreydan
-Description: Uses Pythons infamous eval() function as a way to implement calculator
+Description: Uses Pythons eval() function 
+             as a way to implement calculator
+
+Functions available:
+
++ : addition
+- : subtraction
+* : multiplication
+/ : division
+% : percentage
+sin: sine(rad)
+cos: cosine(rad)
+tan: tangent(rad)
+sqrt: square_root(n)
+pi: 3.141......
 """
 
 import math
 
-
-def calc(k):
-
-    functions = ['sin','cos','tan','sqrt','pi']    
+def main():
     
+    def calc(k):
+
+        functions = ['sin','cos','tan','sqrt','pi']    
+        
+        for i in functions:
+            if i in k.lower():
+                withmath = 'math.' + i
+                k = k.replace(i,withmath)
+        
+        try:
+            k = eval(k)
+        except ZeroDivisionError:
+            print ("Can't divide by 0")
+            exit()
+        except NameError:
+            print ("Invalid input")
+            exit()
+        
+        return k
+
+
+    print ("\nScientific Calculator\nEg: pi * sin(90) - sqrt(81)")
+
+    k = input("\nWhat is ")
+
+    k = k.replace(' ','')
+    k = k.replace('^','**')
+    k = k.replace('=','')
+    k = k.replace('?','')
+    k = k.replace('%','/100')
+
+    print ("\n" + str(calc(k)))
     
-    for i in functions:
-        if i in k.lower():
-            withmath = 'math.' + i
-            k = k.replace(i,withmath)
-    
-    try:
-        k = eval(k)
-    except ZeroDivisionError:
-        print ("Can't divide by 0")
-        exit()
-    except NameError:
-        print ("Invalid input")
-        exit()
-    
-    return k
-
-
-print ("\nScientific Calculator\nEg: pi * sin(90) - sqrt(81)")
-
-k = input("\nWhat is ")
-
-k = k.replace(' ','')
-k = k.replace('^','**')
-k = k.replace('=','')
-k = k.replace('?','')
-
-print ("\n" + str(calc(k)))
+if __name__ == "__main__":
+    main()
