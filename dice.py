ef81a1db9a209eb2a7da6e33fe2552faae5a46a5
angus <1047053860@qq.com>
2017-06-03 01:12:23 +0800

    Add some judgement when changing sides.

--- dice.py
+++ dice.py
@@ -10,11 +10,24 @@
 
 import random
 class Die(object):
-  def __init__(self, sides):
-    self.sides = sides
+  #A dice has a feature of number about how many sides it has when it's established,like 6.
+  def __init__(self):
+    self.sides=6
+    
+  """because a dice contains at least 4 planes.
+  So use this method to give it a judgement when you need to change the instance attributes."""
+  def set_sides(self,sides_change):
+    if self.sides_change>=4:
+      self.sides=self.sides_change
+      print("change sides!")
+    else:
+      print("wrong sides!")
+      
   def roll(self):
     return random.randint(1, self.sides)
 
-d = Die(6)
-d1 = Die(6)
+d = Die()
+d1 = Die()
+d.set_sides(4)
+d1.set_sides(4)
 print (d.roll(), d1.roll())
