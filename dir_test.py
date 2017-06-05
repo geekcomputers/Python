ec27b87ea265d40b9448cc1efe4d8a80af59fcf1
EroMonsterSanji <1002174223@qq.com>
2017-06-06 05:22:37 +0800

    compatible with python3

--- dir_test.py
+++ dir_test.py
@@ -8,12 +8,20 @@
 # Description			: Tests to see if the directory testdir exists, if not it will create the directory for you
 from __future__ import print_function
 import os #Import the OS Module
-CheckDir = raw_input("Enter the name of the directory to check : ")
-print
+import sys
+
+if sys.version_info.major >= 3:
+	input_func = input
+else:
+	input_func = raw_input
+
+CheckDir = input_func("Enter the name of the directory to check : ")
+print()
+
 if os.path.exists(CheckDir):#Checks if the dir exists
 	print("The directory exists")
 else:
 	print("No directory found for "+CheckDir) #Output if no directory
-	print
+	print()
 	os.makedirs(CheckDir)#Creates a new dir for the given name
 	print("Directory created for "+CheckDir)
