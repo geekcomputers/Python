d21124363f2081e4ca148c15cbe4005695de85b8
EroMonsterSanji <1002174223@qq.com>
2017-06-17 19:17:15 +0800

    compatible with python2; PEP8

--- CountMillionCharacters-Variations/variation1.py
+++ CountMillionCharacters-Variations/variation1.py
@@ -1,13 +1,21 @@
-def countChars(filename):
-	count = {}
+import sys
 
-	with open(filename) as info: #inputFile Replaced with filename
-		readFile = info.read()
-		for character in readFile.upper():
-			count[character] = count.get(character, 0) + 1
 
-	return count
+def countchars(filename):
+    count = {}
+
+    with open(filename) as info:  # inputFile Replaced with filename
+        readfile = info.read()
+        for character in readfile.upper():
+            count[character] = count.get(character, 0) + 1
+
+    return count
 
 if __name__ == '__main__':
-	inputFile = input("File Name : ")
-	print(countChars(inputFile))
+    if sys.version_info.major >= 3:  # if the interpreter version is 3.X, use 'input',
+        input_func = input           # otherwise use 'raw_input'
+    else:
+        input_func = raw_input
+
+    inputFile = input_func("File Name : ")
+    print(countchars(inputFile))
