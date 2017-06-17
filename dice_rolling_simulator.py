e42031e71206da221b590ae8576c485158a92f3e
CaptMcTavish <sarmahsubham@gmail.com>
2017-06-17 20:53:43 +0530

    Update dice_rolling_simulator.py
    Someone suggested me not to use while too much, so tried to create an alternative. 
    I am a newbie and this is my first contribution!
    A line in the previous code (The equivalent to which in the current code is line #26 said [10], which should have been [12], please fix it. Thanks.

--- dice_rolling_simulator.py
+++ dice_rolling_simulator.py
@@ -1,98 +1,77 @@
 #Made on May 27th, 2017
 #Made by SlimxShadyx
+#Editted by CaptMcTavish, June 17th, 2017
 
 #Dice Rolling Simulator
 
 import random
 
-#These variables are used for user input and while loop checking.
-correct_word = False
-dice_checker = False
-dicer = False
-roller_loop = False
+global user_exit_checker
+user_exit_checker="exit"
 
-#Checking the user input to start the program.
-while correct_word == False:
+def start():
+    print "Welcome to dice rolling simulator: \nPress Enter to proceed"
+    raw_input(">")
 
-    user_input_raw = raw_input("\r\nWelcome to the Dice Rolling Simulator! We currently support 6, 8, and 12 sided die! \
-    Type [start] to begin!\r\n?>")
+    result()
 
-    #Converting the user input to lower case.
-    user_input = (user_input_raw.lower())
+def bye():
+    print "Thanks for using the Dice Rolling Simulator! Have a great day! =)"
 
-    if user_input == 'start':
-        correct_word = True
-    
+def result():
+
+    user_dice_chooser
+
+
+    print "\r\nGreat! Begin by choosing a die! [6] [8] [12]?\r\n" 
+    user_dice_chooser = raw_input(">")
+
+    user_dice_chooser = int(user_dice_chooser)
+
+    if user_dice_chooser == 6:
+        dice6()
+
+    elif user_dice_chooser == 8:
+        dice8()
+
+    elif user_dice_chooser == 12:
+        dice12()
+
     else:
-        print "Please type [start] to begin!\r\n"
-
-#Main program loop. Exiting this, exits the program.
-while roller_loop == False:
-
-    #Second While loop to ask the user for the certain die they want.
-    while dice_checker == False:
-        user_dice_chooser = raw_input("\r\nGreat! Begin by choosing a die! [6] [8] [10]\r\n?>")
-
-        user_dice_chooser = int(user_dice_chooser)
-
-        if user_dice_chooser == 6:
-            dice_checker = True
-
-        elif user_dice_chooser == 8:
-            dice_checker = True
-
-        elif user_dice_chooser == 12:
-            dice_checker = True
-
-        else:
-            print "\r\nPlease choose one of the applicable options!\r\n"
-    
-    #Another inner while loop. This one does the actual rolling, as well as 
-    #allowing the user to re-roll without restarting the program.
-    while dicer == False:
-
-        if user_dice_chooser == 6:
-            dice_6 = random.randint(1,6)
-            print "\r\nYou rolled a " + str(dice_6) + "!\r\n"
-            dicer = True
-
-            user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
-            user_exit_checker = (user_exit_checker_raw.lower())
-
-            if user_exit_checker == 'roll':
-                dicer = False
-
-            elif user_exit_checker == 'exit':
-                roller_loop = True
+        print "\r\nPlease choose one of the applicable options!\r\n"
+        result()
 
 
-        elif user_dice_chooser == 8:
-            dice_8 = random.randint(1,8)
-            print "\r\nYou rolled a " + str(dice_8) + "!"
-            dicer = True
+def dice6():
+    dice_6 = random.randint(1,6)
+    print "\r\nYou rolled a " + str(dice_6) + "!\r\n"
 
-            user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
-            user_exit_checker = (user_exit_checker_raw.lower())
+    user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
+    user_exit_checker = (user_exit_checker_raw.lower())
+    if user_exit_checker=="roll":
+        start()
+    else:
+        bye()
 
-            if user_exit_checker == 'roll':
-                dicer = False
+def dice8():
+    dice_8 = random.randint(1,8)
+    print "\r\nYou rolled a " + str(dice_8) + "!"
 
-            elif user_exit_checker == 'exit':
-                roller_loop = True
+    user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
+    user_exit_checker = (user_exit_checker_raw.lower())
+    if user_exit_checker=="roll":
+        start()
+    else:
+        bye()
 
-        elif user_dice_chooser == 12:
-            dice_12 = random.randint(1,12)
-            print "\r\nYou rolled a " + str(dice_12) + "!"
-            dicer = True
-            
-            user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
-            user_exit_checker = (user_exit_checker_raw.lower())
+def dice12():
+    dice_12 = random.randint(1,12)
+    print "\r\nYou rolled a " + str(dice_12) + "!"
 
-            if user_exit_checker == 'roll':
-                dicer = False
-
-            elif user_exit_checker == 'exit':
-                roller_loop = True
-
-print "Thanks for using the Dice Rolling Simulator! Have a great day! =)"
-       
+    user_exit_checker_raw = raw_input("\r\nIf you want to roll another die, type [roll]. To exit, type [exit].\r\n?>")
+    user_exit_checker = (user_exit_checker_raw.lower())
+    if user_exit_checker=="roll":
+        start()
+    else:
+        bye()
+start()
