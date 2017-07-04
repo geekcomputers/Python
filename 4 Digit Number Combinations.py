5f9d49d9d05b07a895583276f196911ea8c9332f
ytho <scott.pearce7796+github@gmail.com>
2017-07-04 17:33:34 +1000

    Update 4 Digit Number Combinations.py
    Again, removed unneeded code.

--- 4 Digit Number Combinations.py
+++ 4 Digit Number Combinations.py
@@ -1,8 +1,4 @@
 # ALL the combinations of 4 digit combo
 def FourDigitCombinations():
-    numbers = []
     for code in range(10000):
-        numbers.append(str(code).zfill(4))
-
-    for i in numbers:
-        print(i)
+        print(str(code).zfill(4))
