9e514c9cd174e4c9892760ee769e0dc00cc33b46
RISHAV KUMAR <30614914+RISHAV98@users.noreply.github.com>
2017-08-01 12:17:24 +0530

    Print_List_of_Even_Numbers.py

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,2 +1,10 @@
-print [x for x in range(2, 100) if not x % 2]
-print range(2, 100, 2)
+ def even(maxNum):
+	# Start with empty lists for even numbers
+	evens = []
+  # Loop through every integer up to the input maxNum	
+	for num in range(1, maxNum + 1):
+		# Test if the number is even
+		if num % 2 == 0:
+			evens.append(num)
+      print("Evens:", evens)
+	
