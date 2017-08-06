90f9b8420633aae38727c73cf481f79bf673a85f
Vikas Kumar <24394958+dzenvikas@users.noreply.github.com>
2017-08-06 18:37:04 +0530

    comment added
    More clarification added to the code.

--- SimpleStopWatch.py
+++ SimpleStopWatch.py
@@ -6,7 +6,7 @@ import time
 print('Press ENTER to begin, Press Ctrl + C to stop')
 while True:
     try:
-        input() #For ENTER
+        input() # For ENTER. Use raw_input() if you are running python 2.x instead of input()
         starttime = time.time()
         print('Started')
     except KeyboardInterrupt:
