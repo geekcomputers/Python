ace93171373ab29dd6e869677c998e3ce3a88997
RGauthamRam <30320759+RGauthamRam@users.noreply.github.com>
2017-09-08 20:30:09 +0530

    Update check_input.py
    Testcase variable is not essential.
    start and end parameters are used inside the functions instead of the numbers 1 and 6.

--- check_input.py
+++ check_input.py
@@ -1,12 +1,10 @@
 def get_user_input(start,end):
 
-    testcase = False
-    while testcase == False:
+    while (1):
         try:
             userInput = int(input("Enter Your choice: "))
-            if userInput > 6 or userInput < 1:
+            if userInput > end  or userInput < start:
                 print("Please try again.")
-                testcase = False
             else:
                 return userInput
 
@@ -16,8 +14,6 @@ def get_user_input(start,end):
 
 x = get_user_input(1,6)
 print(x)
-
-
 ###Asks user to enter something, ie. a number option from a menu.
 ###While type != interger, and not in the given range,
 ###Program gives error message and asks for new input.
