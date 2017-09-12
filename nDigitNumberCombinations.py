b3650de628b26d003ffc332392baecb64ba69952
Giftlin <31629119+Giftlin@users.noreply.github.com>
2017-09-13 00:14:24 +0530

    Update nDigitNumberCombinations.py

--- nDigitNumberCombinations.py
+++ nDigitNumberCombinations.py
@@ -1,9 +1,9 @@
 # ALL the combinations of n digit combo
 def nDigitCombinations():
 	try:
-		pow = 10**n
+		npow = 10**n
 		numbers=[]
-		for code in range(pow):
+		for code in range(npow):
 			code=str(code).zfill(n)
 			numbers.append(code)
 	except:
