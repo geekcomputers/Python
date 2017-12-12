f610aba0ce3b436e3b34889383707a4cc9df56f7
screamff <30379611+screamff@users.noreply.github.com>
2017-12-12 11:19:48 +0800

    Corrected format

--- move_files_over_x_days.py
+++ move_files_over_x_days.py
@@ -17,3 +17,4 @@ for f in os.listdir(src):  # Loop through all the files in the source directory
     if os.stat(f).st_mtime < now - 240 * 86400:  # Work out how old they are, if they are older than 240 days old
         if os.path.isfile(f):  # Check it's a file
             shutil.move(f, dst)  # Move the files
+            
