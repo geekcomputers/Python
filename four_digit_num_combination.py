97eb1f2e1f3c1b359bbed756baa50b71f80693d1
luke zhou <sharlock@gmail.com>
2018-01-29 09:10:06 +1100

    adding the original combination function back

--- four_digit_num_combination.py
+++ four_digit_num_combination.py
@@ -1,3 +1,11 @@
+# ALL the combinations of 4 digit combo
+def FourDigitCombinations():
+    numbers=[]
+    for code in range(10000):
+        code=str(code).zfill(4)
+        print(code)
+        numbers.append(code)
+
 # Same as above but more pythonic
 def oneLineCombinations():
     """ print out all 4-digit numbers """
