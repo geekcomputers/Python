4c9d5e0206408f6f123bd1f1fe7c587625153d9e
Christian Bender <christianbender89@web.de>
2018-04-10 17:00:39 +0200

    added main-method and changed the program logic

--- CountMillionCharacters-Variations/variation1.py
+++ CountMillionCharacters-Variations/variation1.py
@@ -1,7 +1,12 @@
 import sys
 
+try:
+    input_func = raw_input
+except:
+    input_func = input
 
-def countchars(filename):
+
+def count_chars(filename):
     count = {}
 
     with open(filename) as info:  # inputFile Replaced with filename
@@ -11,19 +16,18 @@ def countchars(filename):
 
     return count
 
+def main():
+    is_exist=True
+    #Try to open file if exist else raise exception and try again
+    while(is_exist):
+        try:
+            inputFile = input_func("File Name / (0)exit : ")
+            if inputFile == "0":
+                break
+            print(count_chars(inputFile))
+        except FileNotFoundError:
+            print("File not found...Try again!")
+
+
 if __name__ == '__main__':
-    if sys.version_info.major >= 3:  # if the interpreter version is 3.X, use 'input',
-        input_func = input           # otherwise use 'raw_input'
-    else:
-        input_func = raw_input
-
-is_exist=True
-#Try to open file if exist else raise exception and try again
-while(is_exist):
-    try:
-        inputFile = input_func("File Name : ")
-        print(countchars(inputFile))
-        is_exist = False            #Set False if File Name found
-    except FileNotFoundError:
-        print("File not found...Try again!")
-
+    main()
