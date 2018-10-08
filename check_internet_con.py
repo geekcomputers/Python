e075995805e4c3735c5ce711b5b3d6ec3cfd6b1f
Aniruddha09 <aniruddhashinde0807@gmail.com>
2018-10-08 12:56:58 +0530

    Updated check_internet_con.py
    Added function to check internet connectivity.

--- check_internet_con.py
+++ check_internet_con.py
@@ -3,7 +3,7 @@ import urllib2
 
 def checkInternetConnectivity():
     try:
-        urllib2.urlopen("http://google22.com", timeout=2)
+        urllib2.urlopen("http://google.com", timeout=2)
         print("Working connection")
 
     except urllib2.URLError as E:
