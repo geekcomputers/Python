11e49f58ccd0ad1a1eb07e94c96c8b2a5c4583f6
rohitbabugaddeti <rohitbabu.gaddeti@gmail.com>
2018-10-15 19:44:44 +0530

    modified code for readability and security

--- tik_tak.py
+++ tik_tak.py
@@ -1,45 +1,47 @@
+#Tik-tak game
 
-l=["anything",1,2,3,4,5,6,7,8,9]
-i=0
+
+board=["anything",1,2,3,4,5,6,7,8,9]
+switch="p1"
 j=9
 print("\n\t\t\tTIK-TAC-TOE")
-def board():
+def print_board():
     #import os
     #os.system('cls')
     print("\n\n")
     print("    |     |" )
-    print("",l[1]," | ",l[2]," | ",l[3] )
+    print("",board[1]," | ",board[2]," | ",board[3] )
     print("____|_____|____")
     print("    |     |" )
-    print("",l[4]," | ",l[5]," | ",l[6] )
+    print("",board[4]," | ",board[5]," | ",board[6] )
     print("____|_____|____")
     print("    |     |" )
-    print("",l[7]," | ",l[8]," | ",l[9] )
+    print("",board[7]," | ",board[8]," | ",board[9] )
     print("    |     |" )
-def enter_number(p1,p2):
-    global i
+def enter_number(p1_sign,p2_sign):
+    global switch
     global j
     k=9
     while(j):
         if k==0:
             break
         
-        if i==0:
-            x=int(input("\nplayer 1 :- "))
-            if x<=0:
+        if switch=="p1":
+            p1_input=int(input("\nplayer 1 :- "))
+            if p1_input<=0:
                 print("chose number from given board")
             else:
                 for e in range(1,10):
-                    if l[e]==x:
-                        l[e]=p1
-                        board()
+                    if board[e]==p1_input:
+                        board[e]=p1_sign
+                        print_board()
                         c=checkwin()
                         if c==1:
                             print("\n\n Congratulation ! player 1 win ")
                             return
                         
                         
-                        i=1
+                        switch="p2"
                         j-=1
                         k-=1
                         if k==0:
@@ -50,59 +52,60 @@ def enter_number(p1,p2):
             
             break
                    
-        if i==1:
-            y=int(input("\nplayer 2 :- "))
-            if y<=0:
+        if switch=="p2":
+            p2_input=int(input("\nplayer 2 :- "))
+            if p2_input<=0:
                 print("chose number from given board")
                 #return
             else:
                 for e in range(1,10):
-                    if l[e]==y:
-                        l[e]=p2
-                        board()
+                    if board[e]==p2_input:
+                        board[e]=p2_sign
+                        print_board()
                         w=checkwin()
                         if w==1:
                             print("\n\n Congratulation ! player 2 win")
                             return
                         
-                        i=0
+                        switch="p1"
                         j-=1
                         k-=1
                         
                     
 def checkwin():
-    if l[1]==l[2]==l[3]:
+    if board[1]==board[2]==board[3]:
         
         return 1
-    elif l[4]==l[5]==l[6]:
+    elif board[4]==board[5]==board[6]:
         
         return 1
-    elif l[7]==l[8]==l[9]:
+    elif board[7]==board[8]==board[9]:
         
         return 1
-    elif l[1]==l[4]==l[7]:
+    elif board[1]==board[4]==board[7]:
         
         return 1
 
-    elif l[2]==l[5]==l[8]:
+    elif board[2]==board[5]==board[8]:
         
         return 1
-    elif l[3]==l[6]==l[9]:
+    elif board[3]==board[6]==board[9]:
         
         return 1
-    elif l[1]==l[5]==l[9]:
+    elif board[1]==board[5]==board[9]:
         
         return 1
-    elif l[3]==l[5]==l[7]:
+    elif board[3]==board[5]==board[7]:
         
         return 1
     else:
         print("\n\nGame continue")
         
-def main():
-    board()
-    p1=input("\n\nplayer 1 chose your sign [0/x] = ")
-    p2=input("player 2 chose your sign [0/x] = ")
-    enter_number(p1,p2)
+def play():
+    print_board()
+    p1_sign=input("\n\nplayer 1 chose your sign [0/x] = ")
+    p2_sign=input("player 2 chose your sign [0/x] = ")
+    enter_number(p1_sign,p2_sign)
     print("\n\n\t\t\tDeveloped By :- UTKARSH MATHUR")
-main()
+if __name__=="__main__":
+    play()
