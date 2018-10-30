209bf82588612d201a0b8fe8f29e0a02071d6088
Ben McNeill <ben@activeprogrammer.com>
2018-10-29 21:23:14 -0600

    Update Google_News.py
    Added comment to function and updated print feature using the new format syntax.

--- Google_News.py
+++ Google_News.py
@@ -4,7 +4,9 @@ from bs4 import BeautifulSoup as soup
 from urllib.request import urlopen
 
 def news(xml_news_url):
-	
+	'''Print select details from a html response containing xml
+	@param xml_news_url: url to parse	
+	'''
 	Client=urlopen(xml_news_url)
 	xml_page=Client.read()
 	Client.close()
@@ -15,10 +17,10 @@ def news(xml_news_url):
 	
 	for news in news_list:
 		
-		print(news.title.text)
-		print(news.link.text)
-		print(news.pubDate.text)	
-		print("\n\n")
+		print(f'news title:   {news.title.text}')
+		print(f'news link:    {news.link.text}')
+		print(f'news pubDate: {news.pubDate.text}')	
+		print("+-"*20,"\n\n")
 		
 
 
