bcf31f6fa6672cd3b116233cc1f5e4c1e46fdcfb
Jinyoung_Han <33437163+jinyoungHan@users.noreply.github.com>
2018-12-04 05:06:56 +0900

    Update to python3 format
    Change python2 from file to python3 format

--- pscheck.py
+++ pscheck.py
@@ -19,20 +19,20 @@ def ps():
     proginfo = string.split(output)
 
     #display results
-    print "\n\
+    print ("\n\)
     Full path:\t\t", proginfo[5], "\n\
     Owner:\t\t\t", proginfo[0], "\n\
     Process ID:\t\t", proginfo[1], "\n\
     Parent process ID:\t", proginfo[2], "\n\
     Time started:\t\t", proginfo[4]
   except:
-    print "There was a problem with the program." 
+    print ("There was a problem with the program.")
 
 def main():
   if os.name == "posix":											# Unix/Linux/MacOS/BSD/etc
     ps()																	# Call the function
   elif os.name in ("nt", "dos", "ce"):							# if the OS is windows
-    print "You need to be on Linux or Unix to run this"
+    print ("You need to be on Linux or Unix to run this")
 		 
 		 
 if __name__ == '__main__':
