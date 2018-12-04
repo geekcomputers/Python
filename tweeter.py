2cb37c107c8fc7f08c677c2c1d2a793c64f2b32b
Jinyoung_Han <33437163+jinyoungHan@users.noreply.github.com>
2018-12-04 17:08:54 +0900

    Updated to Python 3 this files
    Updated print statements to Python 3

--- tweeter.py
+++ tweeter.py
@@ -21,25 +21,25 @@ def getStatus():
 
 def tweetthis(type):
 	if type == "text":
-		print "Enter your tweet "+user.name
+		print("Enter your tweet "+user.name)
 		tweet = getStatus()
 		try:
 			api.update_status(tweet)
 		except Exception as e:
-			print e
+			print(e)
 			return
 	elif type == "pic":
-		print "Enter pic path "+user.name
+		print("Enter pic path "+user.name)
 		pic = os.path.abspath(raw_input())
-		print "Enter status "+user.name
+		print("Enter status "+user.name)
 		title = getStatus()
 		try:
 			api.update_with_media(pic, status=title)
 		except Exception as e:
-			print e
+			print(e)
 			return
 
-	print "\n\nDONE!!"
+	print("\n\nDONE!!")
 
 def initialize():
 	global api, auth, user
