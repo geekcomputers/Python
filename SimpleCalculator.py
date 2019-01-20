7d9e4aa682aa902262f9fc6ab6fb48db929a097c
Nathan R <NJR4123@gmail.com>
2019-01-19 19:59:20 -0500

    Update and rename SimpleCalculator.json to SimpleCalculator.py

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -13,20 +13,20 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
-choice=input("Enter Choice(1/2/3/4):")
+choice = input("Enter Choice(1/2/3/4):")
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enbter Second number:"))
 
 if choice == '1':
-print(num1,"+",num2,"=", add(num1,num2))
+  print(num1,"+",num2,"=", add(num1,num2))
 
 elif choice == '2':
-print(num1,"-",num2,"=", subtract(num1,num2))
+  print(num1,"-",num2,"=", subtract(num1,num2))
 
 elif choice == '3':
-print(num1,"*",num2,"=", multiply(num1,num2))
+  print(num1,"*",num2,"=", multiply(num1,num2))
 
 elif choice == '4':
-print(num1,"/",num2,"=", divide(num1,num2))
+  print(num1,"/",num2,"=", divide(num1,num2))
 else:
-print("Invalid input")
+  print("Invalid input")
