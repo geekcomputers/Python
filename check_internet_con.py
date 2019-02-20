ae69594e04c3d8c1856b489439c8465284fa8e86
wh1t3fang <35537694+wh1t3fang@users.noreply.github.com>
2019-02-20 02:30:19 -0500

    updated for the python3 urllib merge

--- check_internet_con.py
+++ check_internet_con.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
-import urllib2
 
+try:
+    # For Python 3.0 and later
+    from urllib.request import urlopen
+except ImportError:
+    # Fall back to Python 2's urllib2
+    from urllib2 import urlopen
 
 def checkInternetConnectivity():
     try:
