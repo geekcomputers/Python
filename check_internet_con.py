3aee17816b755fbd4191b010649c7e26a7109c75
cclauss <cclauss@me.com>
2019-03-12 09:37:52 +0100

    Import sort order

--- check_internet_con.py
+++ check_internet_con.py
@@ -2,8 +2,8 @@
 
 try:
     # For Python 3.0 and later
-    from urllib.request import urlopen
     from urllib.error import URLError
+    from urllib.request import urlopen
 except ImportError:
     # Fall back to Python 2's urllib2
     from urllib2 import URLError, urlopen
