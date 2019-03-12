347185af923acca806086d402f94736eb891aea9
cclauss <cclauss@me.com>
2019-03-12 09:36:56 +0100

    Properly import URLError and urlopen in both Python 2 and Python 3
    flake8 testing of https://github.com/geekcomputers/Python on Python 3.7.1
    
    $ __flake8 . --count --select=E9,F63,F72,F82 --show-source --statistics__
    ```
    ./check_internet_con.py:12:9: F821 undefined name 'urllib2'
            urllib2.urlopen("http://google.com", timeout=2)
            ^
    ./check_internet_con.py:15:12: F821 undefined name 'urllib2'
        except urllib2.URLError as E:
               ^
    ```

--- check_internet_con.py
+++ check_internet_con.py
@@ -3,15 +3,15 @@
 try:
     # For Python 3.0 and later
     from urllib.request import urlopen
+    from urllib.error import URLError
 except ImportError:
     # Fall back to Python 2's urllib2
-    from urllib2 import urlopen
+    from urllib2 import URLError, urlopen
 
 def checkInternetConnectivity():
     try:
-        urllib2.urlopen("http://google.com", timeout=2)
+        urlopen("http://google.com", timeout=2)
         print("Working connection")
-
     except urllib2.URLError as E:
         print("Connection error:%s" % E.reason)
 
