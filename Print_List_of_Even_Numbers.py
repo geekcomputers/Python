8803fe4e24f001ef1d74ee3af2c5a1a7f74a1a99
nameet <nameet.nayak@mavs.uta.edu>
2019-07-21 06:19:35 -0500

    Robustness for list of even and odd numbers

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,6 +1,6 @@
 # INPUT NUMBER OF EVEN NUMBERS
 def print_error_messages():
-  print("Invalid number, please enter positive number!")
+  print("Invalid number, please enter a Non-negative number!")
   exit()
 
 
@@ -9,7 +9,7 @@ try:
 except ValueError:
   print_error_messages()
 
-start=2
+start=0
 
 if n < 0:
   print_error_messages()
--- Print_List_of_Odd_Numbers.py
+++ Print_List_of_Odd_Numbers.py
@@ -1,7 +1,18 @@
 # INPUT NUMBER OF ODD NUMBERS
+def print_error_messages():
+  print("Invalid number, please enter a Non-negative number!")
+  exit()
 
-n=int(input('Amount: '))
+
+try:
+  n=int(input('Amount: '))
+except ValueError:
+  print_error_messages()
+
 start=1
+
+if n < 0:
+  print_error_messages()
 
 for i in range(n):
   print(start)
