e7730b81b24ff29d1d8de7c0f1d498707efbd6e2
nameet <nameet.nayak@mavs.uta.edu>
2019-07-21 06:13:09 -0500

    Palindrome and EVEN numbers list, optimized and made robust

--- Palindrome_Checker.py
+++ Palindrome_Checker.py
@@ -8,15 +8,25 @@ samplePhrase = "A man, a plan, a cat, a ham, a yak, a yam, a hat, a canal-Panama
 
 givenPhrase = input("\nPlease input a phrase:(Press ENTER to use the sample phrase) ")
 
-if givenPhrase == "" or not givenPhrase.strip():
+if givenPhrase == "":
     print("\nThe sample phrase is: {0}".format(samplePhrase))
     phrase = samplePhrase
 else:
     phrase = givenPhrase
 
-string = phrase.lower()
+phrase = phrase.lower()
 
-if string == string[::-1]:
+
+length_ = len(phrase)
+bol_ = True
+
+# check using two pointers, one at beginning
+# other at the end. Use only half of the list.
+for items in range(length_//2):
+    if phrase[items] != phrase[length_ - 1 - items]:
+        print("\nSorry, The given phrase is not a Palindrome.")
+        bol_ = False
+        break
+
+if bol_:
     print("\nWow!, The phrase is a Palindrome!")
-else:
-    print("\nSorry, The given phrase is not a Palindrome.")
--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,7 +1,18 @@
 # INPUT NUMBER OF EVEN NUMBERS
+def print_error_messages():
+  print("Invalid number, please enter positive number!")
+  exit()
 
-n=int(input('Amount: '))
-start=0
+
+try:
+  n=int(input('Amount: '))
+except ValueError:
+  print_error_messages()
+
+start=2
+
+if n < 0:
+  print_error_messages()
 
 for i in range(n):
   print(start)
