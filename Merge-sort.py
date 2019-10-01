f9a710cfb79447f8c1cb73ea8e20c796e33e708b
Manthan Gupta <42516515+Manthan109@users.noreply.github.com>
2019-10-01 21:18:15 +0530

    Added comments

--- Merge-sort.py
+++ Merge-sort.py
@@ -10,38 +10,38 @@ for i in range(n):
 
 def merge_sort(L):
 
-    mid = int( (len(L) - 1)/2 )
+    mid = int( (len(L) - 1)/2 )  #calculated value of middle index of array
 
-    if len(L) > 2:
-        a = merge_sort(L[0:mid])
-        b = merge_sort(L[mid:len(L)])
+    if len(L) > 2: #this loop will run when length of array is greater than 2
+        a = merge_sort(L[0:mid]) #a is initialised with left side of the array taking reference as middle index
+        b = merge_sort(L[mid:len(L)]) #b is initialised with right side of the array taking reference as middle index
 
-    elif len(L) == 2:
-        a = [L[0]]
-        b = [L[1]]
+    elif len(L) == 2: #this loop will when length of array is equal to 2
+        a = [L[0]] #a is initiliased with the first element of array 
+        b = [L[1]] #b is initiliased with the second element of array
     else:
         return L
     
     i = 0
     j = 0
     new = []
-    while (i!= len(a) or j != len(b)):
+    while (i!= len(a) or j != len(b)): #this loop will run until i is not equal to the lenth of array a or j is equal to length of array b
 
-        if i < len(a) and j < len(b):
+        if i < len(a) and j < len(b): #checking if value of i and j is lesser than length of array a and b respectively
 
-            if a[i] < b[j]:
-                new += [a[i]]
-                i += 1
+            if a[i] < b[j]:  #if the element on the left side is lesser than the element on the right side
+                new += [a[i]] #then it will be directly added to the array new
+                i += 1 #i is increased by 1
                 
             else:
-                new +=[b[j]]
-                j += 1
+                new +=[b[j]] #if element on the right side is lesser than the element on the left side then right side element is added to the array new
+                j += 1 #j increased by 1
 
-        elif i == len(a) and j != len(b):
+        elif i == len(a) and j != len(b): #if i gets equal to the length of array a then all the elements of array b are directly added to the array new
             new += [b[j]]
             j += 1
             
-        elif j == len(b) and i != len(a):
+        elif j == len(b) and i != len(a):#if j gets equal to the length of array b then all the elements of array a are directly added to the array new
             new += [a[i]]
             i += 1
 
