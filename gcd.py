cab33933e7781f8980b82a32acc8f2be5cd61b0b
ethfar <37633114+Biswa24@users.noreply.github.com>
2019-10-02 21:47:33 +0530

    Create gcd.py
    Computing GCD

---
+++ gcd.py
@@ -0,0 +1,16 @@
+"""
+This function takes two variable and returns greatest common divisior
+"""
+def find_gcd(x, y): 
+  while(y): 
+    x, y = y, x % y 
+    
+  return x 
+  
+  # Input from user
+  print("For computing gcd of two numbers")
+  a,b = map(int,input("Enter the number by comma separating :-",end = " ").split(","))
+ 
+  # Map typecast the input in 'int' type
+  
+  print("Gcd of {} & {} is {}",format(a,b,find_gcd(a,b)))
