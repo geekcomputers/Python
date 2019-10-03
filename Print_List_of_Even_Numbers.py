488a5dbeb5310a4ada3fdbba66819e562a09ab32
shivam agarwal <43515429+shivamagarwal1999@users.noreply.github.com>
2019-10-03 13:00:04 +0530

    Update Print_List_of_Even_Numbers.py

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -14,6 +14,13 @@ start=0
 if n < 0:
   print_error_messages()
 
-for i in range(n): #loop till the number n entered by the user
-  print(start) #prints 0 first as it is an even number
-  start+=2 #increases the value of start by 2 to get to the next even number
+for i in range(n):#loop till the number n entered by the user
+  if(n==0):#checking if it is 0 or not
+    print(start) #prints 0 first as it is an even number
+    start+=2 #increases the value of start by 2 to get to the next even number
+  else:
+    if(n%i==0):#if the number is greater than 0
+      print(start)#print the even numbers
+      
+    
+ 
