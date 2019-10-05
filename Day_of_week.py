06aac94fe0b40d7a043bac238236abde9172c3f7
Chayan <55481820+Chayan-19@users.noreply.github.com>
2019-10-05 16:59:44 +0530

    Create Day_of_week.py

---
+++ Day_of_week.py
@@ -0,0 +1,12 @@
+# Python program to Find day of 
+# the week for a given date 
+import datetime 
+import calendar 
+
+def findDay(date): 
+	born = datetime.datetime.strptime(date, '%d %m %Y').weekday() 
+	return (calendar.day_name[born]) 
+
+# Driver program 
+date = '03 02 2019'
+print(findDay(date)) 
