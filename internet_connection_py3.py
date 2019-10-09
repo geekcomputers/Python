a135a4cda7486e08ea24fd288fcce60465d2e850
Pierrick <laurent.pck@gmail.com>
2019-10-09 19:19:30 +0200

    Use urllib instead of urllib2 and input instead of raw_input function.

--- internet_connection_py3.py
+++ internet_connection_py3.py
@@ -1,22 +1,22 @@
 from __future__ import print_function
-import urllib2
+import urllib.request
 import os
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 print("Testing Internet Connection")
 print()
 try:
-    urllib2.urlopen("http://google.com", timeout=2)#Tests if connection is up and running
+    urllib.request.urlopen("http://google.com", timeout=2)#Tests if connection is up and running
     print("Internet is working fine!")
     print()
-    question = raw_input("Do you want to open a website? (Y/N): ")
+    question = input("Do you want to open a website? (Y/N): ")
     if question == 'Y':
     	print()
-    	search = raw_input("Input website to open (http://website.com) : ")
+    	search = input("Input website to open (http://website.com) : ")
     else:
     	os._exit(0)
 
-except urllib2.URLError:
+except urllib.error.URLError:
     print ("No internet connection!")#Output if no connection
 
 browser = webdriver.Firefox()
