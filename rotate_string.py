f663572d72e9cd01740a1a36bee4d914e807e02c
Aditya Bhogate <43694876+aadi05@users.noreply.github.com>
2019-10-12 13:04:21 +0530

    rotate a string
    rotate string in left, right and circular direction.

---
+++ rotate_string.py
@@ -0,0 +1,21 @@
+def left_rotate(s,val):
+        s1 = s[0:val]
+        s2 = s[val:]
+        return s2+s1
+
+def right_rotate(s,val):
+        s1 = s[0:len(s)-val]
+        s2 = s[len(s)-val:]
+        return s2+s1
+
+def circular_rotate(s):
+        s = list(s)
+        idx = 0
+        mid = len(s)//2
+        for i in reversed(range(mid,len(s))):
+                s[idx],s[i] = s[i],s[idx]
+                idx += 1
+        return s
+
+s = 'aditya'
+print(''.join(circular_rotate(s)))
