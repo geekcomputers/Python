b6b973feaea5e703138b85a0db11592f7aebea75
Abhishek garg <33170757+abhi01274@users.noreply.github.com>
2019-10-20 16:05:49 +0530

    Update Merge-sort.py

--- Merge-sort.py
+++ Merge-sort.py
@@ -10,7 +10,7 @@ for i in range(n):
 
 
 def merge_sort(L):
-    mid = int((len(L) - 1) / 2)  # calculated value of middle index of array
+    mid = int((len(L) - 1) / 2)  # calculate the value of middle index of array
 
     if len(L) > mid + 2:  # this loop will run when length of array is greater than 2
         a = merge_sort(L[0:mid])  # a is initialised with left side of the array taking reference as middle index
