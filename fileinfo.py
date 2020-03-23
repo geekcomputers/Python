e729208ea0080b9285aff0ef43790e59e2c2bb42
Alopex <alopex4@163.com>
2020-03-23 19:03:24 +0800

    Unify the information print out way
    Listen to @MichaelCurrin opinion.
        - remove the not use line.
        - use tuple to hold the items

--- fileinfo.py
+++ fileinfo.py
@@ -1,11 +1,11 @@
-# Script Name       : fileinfo.py
+ Script Name       : fileinfo.py
 # Author            : Not sure where I got this from
 # Created           : 28th November 2011
 # Last Modified     : 23th March 2020
 # Version           : 1.0
 # Modifications     :
 
-# Description       : Show file information for a given file
+# Description           : Show file information for a given file
 
 # get file information using os.stat()
 # tested with Python24 vegsaeat 25sep2006
@@ -15,13 +15,11 @@ import os
 import stat  # index constants for os.stat()
 import sys
 import time
-from collections import OrderedDict
 
 if sys.version_info >= (3, 0):
     raw_input = input
 
-# pick a file you have
-file_name = raw_input("Enter a file name: ")  
+file_name = raw_input("Enter a file name: ")  # pick a file you have
 count = 0
 t_char = 0
 
@@ -55,32 +53,30 @@ file_info = {
     'no_of_lines': count,
     't_char': t_char
 }
+# print out the file info
+file_info_keys = ('file name', 'file size', 'last modified', 'last accessed',
+                  'creation time', 'Total number of lines are',
+                  'Total number of characters are')
+file_info_vales = (file_info['fname'], str(file_info['fsize']) + " bytes",
+                   file_info['f_lm'], file_info['f_la'], file_info['f_ct'],
+                   file_info['no_of_lines'], file_info['t_char'])
 
-file_info_fmt = ('\nfile name = {}\nfile size = {}\nlast modified = {}\n'
-                 'last accessed = {}\ncreation time = {}\n'
-                 'Total number of lines are = {}\n'
-                 'Total number of characters are = {}')
-print(
-    file_info_fmt.format(file_info['fname'],
-                         str(file_info['fsize']) + " bytes", file_info['f_lm'],
-                         file_info['f_la'], file_info['f_ct'],
-                         file_info['no_of_lines'], file_info['t_char']))
+for f_key, f_value in zip(file_info_keys, file_info_vales):
+    print(f_key, ' =', f_value)
 
+# check the `file` is direcotry
+# print out the file stats
 if stat.S_ISDIR(file_stats[stat.ST_MODE]):
-    print("This a directory")
+    print("This a directory.")
 else:
     file_stats_fmt = ''
     print("\nThis is not a directory.")
-    stats_strings = ("st_mode (protection bits),"
-                     "st_ino (inode number),"
-                     "st_dev (device),"
-                     "st_nlink (number of hard links),"
-                     "st_uid (user ID of owner),"
-                     "st_gid (group ID of owner),"
-                     "st_size (file size bytes),"
-                     "st_atime (last access time seconds since epoch),"
-                     "st_mtime (last modification time),"
-                     "st_ctime (time of creation Windows)")
-    stats_keys = stats_strings.split(',')
-    for key, value in OrderedDict(zip(stats_keys, file_stats)).items():
-        print(key, ' =', value)
+    stats_keys = ("st_mode (protection bits)", "st_ino (inode number)",
+                  "st_dev (device)", "st_nlink (number of hard links)",
+                  "st_uid (user ID of owner)", "st_gid (group ID of owner)",
+                  "st_size (file size bytes)",
+                  "st_atime (last access time seconds since epoch)",
+                  "st_mtime (last modification time)",
+                  "st_ctime (time of creation Windows)")
+    for s_key, s_value in zip(stats_keys, file_stats):
+        print(s_key, ' =', s_value)
