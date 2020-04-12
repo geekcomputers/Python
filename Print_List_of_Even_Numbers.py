b13d18d4bbc1464f6bef38c601e48eb4e0d67cbb
Prince Gangurde <50592495+Prince326@users.noreply.github.com>
2020-04-13 02:03:43 +0530

    Update Print_List_of_Even_Numbers.py
    reduced the code . i think its looks little simple now

--- Print_List_of_Even_Numbers.py
+++ Print_List_of_Even_Numbers.py
@@ -1,15 +1,9 @@
 # INPUT NUMBER OF EVEN NUMBERS
-def print_error_messages():  # function to print error message if user enters a negative number
-    print("Invalid number, please enter a Non-negative number!")
-    exit()
 
-
-n = int(input())  # user input
-
+n = int(input("Enter the limit : "))  # user input
 
 if n < 0:
-    print_error_messages()
-
-
-result = ''.join(str(i)+"\n" for i in range(0,n+1,2))  # creating string with number "i"
-print(result)                                          # in range from 0 till "n" with step 2;
+    print("Invalid number, please enter a Non-negative number!")
+else:    
+    even_list = [i for i in range(0,n+1,2)]         # creating string with number "i"
+    print(even_list)                                # in range from 0 till "n" with step 2
