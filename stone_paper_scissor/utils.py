eafaaaa2fc6ec7be68cbb97c8e2d3d1a2d7f80d4
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-07-25 16:37:30 +0530

    Stone Paper Scissor game added

---
+++ stone_paper_scissor/utils.py
@@ -0,0 +1,20 @@
+def validate(hand):
+    if hand < 0 or hand > 2:
+        return False
+    return True
+
+def print_hand(hand, name='Guest'):
+    hands = ['Rock', 'Paper', 'Scissors']
+    print(name + ' picked: ' + hands[hand])
+
+def judge(player, computer):
+    if player == computer:
+        return 'Draw'
+    elif player == 0 and computer == 1:
+        return 'Lose'
+    elif player == 1 and computer == 2:
+        return 'Lose'
+    elif player == 2 and computer == 0:
+        return 'Lose'
+    else:
+        return 'Win'
