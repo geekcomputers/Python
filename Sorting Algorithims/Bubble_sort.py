abd0f82adffdb639ed62dfc107767137a4742006
Mohit Kumar <47894634+Mohitkumar6122@users.noreply.github.com>
2020-08-15 19:03:00 +0530

    Created Bubble Sort

---
+++ Sorting Algorithims/Bubble_sort.py
@@ -0,0 +1,17 @@
+def bubble_sort(nums):
+    # We set swapped to True so the loop looks runs at least once
+    swapped = True
+    while swapped:
+        swapped = False
+        for i in range(len(nums) - 1):
+            if nums[i] > nums[i + 1]:
+                # Swap the elements
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+                # Set the flag to True so we'll loop again
+                swapped = True
+
+
+# Verify it works
+random_list_of_nums = [5, 2, 1, 8, 4]
+bubble_sort(random_list_of_nums)
+print(random_list_of_nums)
