00bb5ffffcd8979a815a6a1288db32c0774a1376
Ayush Kumar <71185961+ayushkumar0208@users.noreply.github.com>
2020-09-21 22:52:21 +0530

    Add files via upload

---
+++ Factorials from 1 to N.py
@@ -0,0 +1,12 @@
+num = int(input("Enter Value of N :"))
+i=1
+while(i<=num):
+    n=i
+    a=1
+    while(n>=1):
+        a=a*n
+        n=n-1
+    print("The factorial of",i,"is",a)
+    i=i+1
+    
+
