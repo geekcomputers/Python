ea96f65e2e6a819d8d2391fda7c43a9f8d8914e4
Swastik Saha <65535891+Swastik-Saha@users.noreply.github.com>
2020-10-01 17:15:00 +0530

    Update Guessing_Game.py
    Added the function to count the number of chances user has used to guess the number correctly.

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,6 +1,7 @@
 import random
 
 a = comGuess = random.randint(0, 100)  # a and comGuess is initialised with a random number between 0 and 100
+n = 0 # Initialising the variable n with 0 which represents the number of chances user have used.
 
 while True:  # loop will run until encountered with the break statement(user enters the right answer)
     userGuess = int(input("Enter your guessed no. b/w 0-100:"))  # user input for guessing the number
@@ -8,12 +9,15 @@ while True:  # loop will run until encountered with the break statement(user ent
         print("Guess Higher")
         comGuess = random.randint(a, 100)
         a += 1
+        n += 1
 
     elif userGuess > comGuess:  # if number guessed by user is greater than the random number than the user is told to guess lower and the random number comGuess is changed to a new random number between 0 and a
         print("Guess Lower")
         comGuess = random.randint(0, a)
         a += 1
+        n += 1
 
     else:  # if guessed correctly the loop will break and the player will win
         print("Guessed Corectly")
+        print(f"You used {n} chances to guess it correctly."
         break
