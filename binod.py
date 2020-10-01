2a4a8c9f07f0eb39c583e9e52aa4c4a66cdc3dca
anand722000 <59029929+anand722000@users.noreply.github.com>
2020-10-01 10:40:30 +0530

    Update binod.py

--- binod.py
+++ binod.py
@@ -1,8 +1,8 @@
-import os
+import os    #The OS module in python provides functions for interacting with the operating system
 
 
-def checkBinod(file):
-    with open(file, "r") as f:
+def checkBinod(file):       #this function will check there is any 'Binod' text in file or not
+    with open(file, "r") as f: #we are opening file in read mode and using 'with' so need to take care of close()
         fileContent = f.read()
     if 'binod' in fileContent.lower():
         print(
