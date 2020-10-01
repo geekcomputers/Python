c787151fb30a79f4860028f9d55f9cacf49e990c
k.v.vora <58413260+krupalvora@users.noreply.github.com>
2020-10-01 09:18:18 +0530

    upadted jarvis.py
    by adding voice output

--- JARVIS/JARVIS.py
+++ JARVIS/JARVIS.py
@@ -9,7 +9,7 @@ JARVIS:
 # import modules
 from datetime import datetime  # datetime module supplies classes for manipulating dates and times
 import subprocess  # subprocess module allows you to spawn new processes
-
+from playsound import *  #for sound output
 import speech_recognition as sr  # speech_recognition Library for performing speech recognition with support for Google Speech Recognition, etc..
 
 # pip install pyttsx3                   # need to run only once to install the library
@@ -26,7 +26,14 @@ with sr.Microphone() as source:
     engine.say("Say something")
     engine.runAndWait()
     audio = r.listen(source)
-
+    
+    
+#for audio output instead of print
+def voice(p):
+    myobj=gTTS(text=p,lang='en',slow=False)
+    myobj.save('try.mp3')
+    playsound('try.mp3')
+    
 # recognize speech using Google Speech Recognition
 Query = r.recognize_google(audio, language = 'en-IN', show_all = True )
 print(Query)
@@ -36,6 +43,8 @@ print(Query)
 def get_app(Q):
     if Q == "time":
         print(datetime.now())
+        x=datetime.now()
+        voice(x)
     elif Q == "notepad":
         subprocess.call(['Notepad.exe'])
     elif Q == "calculator":
