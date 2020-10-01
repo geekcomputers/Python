71b808dc4d07cd2f5f5b64bdfe4ea6ce04a337ca
sahik10 <67615603+sahik10@users.noreply.github.com>
2020-10-01 18:00:09 +0530

    update the program and wrote again
    edited the whole program and wrote the whole program

--- To print series 1,12,123,1234......py
+++ To print series 1,12,123,1234......py
@@ -1,10 +1,37 @@
-i=1
-n=4
-while(i<=n):
-    j=1
-    while(j<=i):
-        print("*",end = ' ')
-        j=j+1
-    print()
-    i=i+1
-    
+def num(a):  
+
+       # initialising starting number  
+
+   num = 1
+
+ # outer loop to handle number of rows  
+
+   for i in range(0, a):  
+
+    # re assigning num  
+
+       num = 1
+
+ # inner loop to handle number of columns  
+
+           # values changing acc. to outer loop  
+
+       for k in range(0, i+1):  
+
+         # printing number  
+
+           print(num, end=" ")  
+
+            # incrementing number at each column  
+
+           num = num + 1    
+
+# ending line after each row  
+
+       print("\r")  
+
+# Driver code
+
+a = 5
+
+num(a)  
