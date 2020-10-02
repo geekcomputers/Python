49f834d9084bc1a57a7ce52fc414f7368070b4db
A1bhinavSingh <64210967+A1bhinavSingh@users.noreply.github.com>
2020-10-02 21:43:34 +0530

    Update SimpleCalculator.py

--- SimpleCalculator.py
+++ SimpleCalculator.py
@@ -15,6 +15,9 @@ def divide(a, b):
         return a / b
     except ZeroDivisionError:
         return "Zero Division Error"
+    
+ def power(a,b):
+    return a**b
 
 def main():
     print("Select Operation")
@@ -22,8 +25,9 @@ def main():
     print("2.Subtract")
     print("3.Multiply")
     print("4.Divide")
+    print("5.Power")
 
-    choice = input("Enter Choice(+,-,*,/): ")
+    choice = input("Enter Choice(+,-,*,/,**): ")
     num1 = int(input("Enter first number: "))
     num2 = int(input("Enbter Second number:"))
 
@@ -38,6 +42,8 @@ def main():
 
     elif choice == '/':
         print(num1, "/", num2, "=", divide(num1, num2))
+     elif choice =="**":
+        print(num1,"^",num2,"=",power(num1,num2))
     else:
         print("Invalid input")
         main()
