fc45485bc058edc8ec104a1f48156865e8d4a608
manitajay <54741067+manitajay@users.noreply.github.com>
2020-10-02 22:04:32 +0530

    Update Cricket_score.py

--- Cricket_score.py
+++ Cricket_score.py
@@ -1,9 +1,12 @@
 from urllib import request
+import os 
+import pyttsx3 
 
 import bs4  # Beautiful Soup for Web Scraping
 from win10toast import ToastNotifier
 
 toaster = ToastNotifier()
+#url from where we extrat data
 
 url = "http://www.cricbuzz.com/cricket-match/live-scores"
 
@@ -20,3 +23,13 @@ for result in soup.find_all('div', attrs={"class": "cb-lv-scrs-col cb-text-compl
 
 print(score[0], results[0])
 toaster.show_toast(title=score[0], msg=results[0])
+
+  
+# initialisation 
+engine = pyttsx3.init() 
+  
+# testing 
+engine.say("match score is"score[0]) 
+engine.say("match won by" results[0) 
+engine.runAndWait()
+#after my update now this program speaks
