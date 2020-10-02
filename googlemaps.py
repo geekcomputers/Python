47174dc9f0c5225371a02900c888f96433056ca7
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-10-02 08:49:48 +0530

    Added Google Map Finder

---
+++ googlemaps.py
@@ -0,0 +1,20 @@
+import requests
+import json
+import geocoder
+
+g = geocoder.ip('me')
+
+lat = g.latlng[0]
+
+longi = g.latlng[1]
+
+https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDqWyJzDN21MlLdS_PHqN2GQe_YCUHz6Q8
+def find(query):
+    key = "AIzaSyDqWyJzDN21MlLdS_PHqN2GQe_YCUHz6Q8"
+    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + str(lat) + "," + str(longi) + "radius=1000"
+
+    r = requests.get(url + 'query=' + query + '&key=' + key)
+
+    x = r.json()
+    y = x['results']
+    print(y)
