698477b4e5db2600174b961dd686fb081a38a3f8
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-10-02 09:08:40 +0530

    Added Google Maps

--- googlemaps.py
+++ googlemaps.py
@@ -7,14 +7,14 @@ g = geocoder.ip('me')
 lat = g.latlng[0]
 
 longi = g.latlng[1]
+query = input("Enter the query")
 
-https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDqWyJzDN21MlLdS_PHqN2GQe_YCUHz6Q8
-def find(query):
-    key = "AIzaSyDqWyJzDN21MlLdS_PHqN2GQe_YCUHz6Q8"
-    url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + str(lat) + "," + str(longi) + "radius=1000"
+key = "your_api_key"
+url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + \
+    str(lat) + "," + str(longi) + "radius=1000"
 
-    r = requests.get(url + 'query=' + query + '&key=' + key)
+r = requests.get(url + 'query=' + query + '&key=' + key)
 
-    x = r.json()
-    y = x['results']
-    print(y)
+x = r.json()
+y = x['results']
+print(y)
