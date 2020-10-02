24c04d28f43bff77a6a59324e77412519440a1c8
Gaurav Singh <55052983+GauravSingh9356@users.noreply.github.com>
2020-10-02 09:04:44 +0530

    Update googlemaps.py

--- googlemaps.py
+++ googlemaps.py
@@ -10,7 +10,7 @@ longi = g.latlng[1]
 
 https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour&key=AIzaSyDqWyJzDN21MlLdS_PHqN2GQe_YCUHz6Q8
 def find(query):
-    key = "AIzaSyDqWyJzDN21MlLdS_PHqN2GQe_YCUHz6Q8"
+    key = "your_api_key"
     url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + str(lat) + "," + str(longi) + "radius=1000"
 
     r = requests.get(url + 'query=' + query + '&key=' + key)
