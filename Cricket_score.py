beee78b0e8a592aa6f950cafdaf8c11b99bf4a68
manitajay <54741067+manitajay@users.noreply.github.com>
2020-10-02 22:08:51 +0530

    Update Cricket_score.py

--- Cricket_score.py
+++ Cricket_score.py
@@ -20,16 +20,18 @@ for div_tags in soup.find_all('div', attrs={"class": "cb-lv-scrs-col text-black"
     score.append(div_tags.text)
 for result in soup.find_all('div', attrs={"class": "cb-lv-scrs-col cb-text-complete"}):
     results.append(result.text)
-
-print(score[0], results[0])
-toaster.show_toast(title=score[0], msg=results[0])
-
-  
-# initialisation 
+    
 engine = pyttsx3.init() 
   
 # testing 
-engine.say("match score is"score[0]) 
-engine.say("match won by" results[0) 
+engine.say("match score and result is")
+print(score[0], results[0])
+toaster.show_toast(title=score[0], msg=results[0]) 
 engine.runAndWait()
+
+
+
+  
+# initialisation 
+
 #after my update now this program speaks
