404da200b4ea7ffb9d12761ef60afcc758f215e5
anshu189 <65893784+anshu189@users.noreply.github.com>
2020-10-02 23:27:46 +0530

    Added a necessary line of code
    Added code: It returns the number of times the odd numbers are counted.

--- Print_List_of_Odd_Numbers.py
+++ Print_List_of_Odd_Numbers.py
@@ -6,4 +6,5 @@ if n <= 0:
     print("Invalid number, please enter a number greater than zero!")
 else:    
     odd_list = [i for i in range(1,n+1,2)]      # creating string with number "i"
-    print(odd_list)                             # in range from 1 till "n".
+    length = len(odd_list)
+    print(odd_list, "\n Total count of odd numbers:", length)                             # in range from 1 till "n".
