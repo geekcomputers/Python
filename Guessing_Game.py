d93acfa98d14d4324547095299237cacca3a0e9c
anshu189 <65893784+anshu189@users.noreply.github.com>
2020-10-03 17:56:56 +0530

    Update Guessing_Game.py
    Try to make the code simpler and instead of importing the whole module I imported the "randint" method and the same with the time module.
    As result, the program will be compiled in the possible least execution time.
    Thank you : )

--- Guessing_Game.py
+++ Guessing_Game.py
@@ -1,7 +1,14 @@
 from random import randint
+from time import sleep
 
+print("Hello Welcome To The Guess Game!")
+sleep(1)
+print("I\'m Geek! What's Your Name?")
+name = input()
+sleep(1)
+print(f"Okay {name} Let's Begin The Guessing Game!")
 a = comGuess = randint(0, 100)  # a and comGuess is initialised with a random number between 0 and 100
-
+count = 0
 while True:  # loop will run until encountered with the break statement(user enters the right answer)
     userGuess = int(input("Enter your guessed no. b/w 0-100:"))  # user input for guessing the number
     
@@ -9,11 +16,16 @@ while True:  # loop will run until encountered with the break statement(user ent
         print("Guess Higher")
         comGuess = randint(a, 100)
         a += 1
+        count = 1
 
     elif userGuess > comGuess:  # if number guessed by user is greater than the random number than the user is told to guess lower and the random number comGuess is changed to a new random number between 0 and a
         print("Guess Lower")
         comGuess = randint(0, a)
         a += 1
+        count = 1
+        
+    elif userGuess == comGuess and count == 0:  # the player needs a special reward for perfect guess in the first try ;-)
+        print("Bravo! Legendary Guess!")
     
     else:  #Well, A Congratulations Message For Guessing Correctly!
         print("Congratulations, You Guessed It Correctly!")
