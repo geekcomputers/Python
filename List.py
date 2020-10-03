74aac76057ef1946b1ef7723ab387d3babcc4cc9
mohit batra <mohitbatra888@gmail.com>
2020-10-03 19:42:33 +0530

    Added some more functionality in the list code

--- List.py
+++ List.py
@@ -1,7 +1,7 @@
-List = []
+List = [5,6,8,9,5,9,0,5,3,0,7,8]
 # List is Muteable
 # means value can be change
-List.insert(0 , 5)
+List.insert(0 ,5)
 List.insert(1,10)
 List.insert(0,6)
 print(List)
@@ -13,6 +13,11 @@ print(List)
 List.pop()
 List.reverse()
 print(List)
+
+
+#slicling in the list
+
+list[2:5] #it will return all the value from index 2 to index 4
 """
 List.append(1)
 print(List)
