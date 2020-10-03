92b7edf8478823e13ee80de03d180f74b26c07f4
Kalyani Ingle <57034765+kalyaniingle@users.noreply.github.com>
2020-10-03 14:44:07 +0530

    Improve Array Printing

--- Quick_Sort.py
+++ Quick_Sort.py
@@ -45,8 +45,8 @@ def quickSort(arr, low, high):
 
 
 arr = [10, 7, 8, 9, 1, 5]
+print("Initila array is:", arr)
 n = len(arr)
 quickSort(arr, 0, n - 1)
-print("Sorted array is:")
-for i in range(n):
-    print("%d" % arr[i]),
+print("Sorted array is:", arr)
+
