58de3d7c791619af13f55b0f2820e872cb4f55c9
Punnatt Punnotok <punnattnp@Punnatts-MacBook-Pro.local>
2020-10-03 17:52:30 +0700

    Added lcm.py

--- lcm.py
+++ lcm.py
@@ -1,17 +1,17 @@
 # Python program to find the L.C.M. of two numbers from input
 
 def lcm(x, y):
-   if x > y:
-       greater_number = x
-   else:
-       greater_number = y
-   while(True):
-       if((greater_number % x == 0) and (greater_number % y == 0)):
-           lcm = greater_number
-           break
-       greater_number += 1
-   return lcm
-
+    if x > y:
+        greater_number = x
+    else:
+        greater_number = y
+    while(True):
+        if((greater_number % x == 0) and (greater_number % y == 0)):
+            lcm = greater_number
+            break
+        greater_number += 1
+    return lcm
+    
 num_1 = int(input('Enter first number: '))
 num_2 = int(input('Enter second number: '))
 
