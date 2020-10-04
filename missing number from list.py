31be3baaec6a94e5aa0c170e44a4b289d2ffe4c6
Slayking1965 <69784108+Slayking1965@users.noreply.github.com>
2020-10-04 16:36:37 +0530

    missing number from list.py
    A simple program to find missing number from a list. Sir/ma'am, please accept my pull request

---
+++ missing number from list.py
@@ -0,0 +1,8 @@
+#program to find a missing number from a list.
+
+def missing_number(num_list):
+    return sum(range(num_list[0],num_list[-1]+1)) - sum(num_list)
+
+print(missing_number([1,2,3,4,6,7,8]))  
+
+print(missing_number([10,11,12,14,15,16,17]))
