a6c9e090d6da8f94cb1622d05bb99f94efabf085
Aditya Mohan <54040096+TraXIcoN@users.noreply.github.com>
2020-10-05 02:49:46 +0530

    Update json_to_table.py

--- json_to_table.py
+++ json_to_table.py
@@ -1,9 +1,8 @@
-//uncomment below line to test the code
-//jsondata = '{"0001":{"FirstName":"John","LastName":"Mark","MiddleName":"Lewis","username":"johnlewis2","password":"2910"}}'
-
-//Used panndas dataframe to convert json data to dataframes table
 import json
 import pandas as pd
+//jsondata is a collection of data in json format
+
+//Used panndas dataframe to convert json data to dataframes table
 jdata = json.loads(jsondata)
 df = pd.DataFrame(jdata)
-print(df.T)
+//print(df.T)
