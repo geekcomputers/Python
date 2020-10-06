8b69c4f7b212d851490712f8d7b0f01048f830d7
NiharikaNeha <45658446+NiharikaNeha@users.noreply.github.com>
2020-10-06 11:53:15 +0530

    Avg_xdspam_confidence.py
    This python program asks for a file name as an input. The sample files are:
    mbox-short.txt: Can be downloaded from the link: https://www.py4e.com/code3/mbox-short.txt
    mbox.txt: Can be downloaded from the link: https://www.py4e.com/code3/mbox.txt
    It displays the average X-DSPAM Confidence value from the file.
    The line format from which this information is extracted from the file is 'X-DSPAM-Confidence: 0.8475'
    This program uses the concepts of lists, file management and string handling.

---
+++ avg_xdspam_confidence.py
@@ -0,0 +1,12 @@
+fh = open('mbox-short.txt')
+#The 'mbox-short.txt' file can be downloaded from the link: https://www.py4e.com/code3/mbox-short.txt
+sum = 0
+count = 0
+for fx in fh:
+	fx = fx.rstrip()
+	if not fx.startswith("X-DSPAM-Confidence:") :
+		continue
+	fy = fx[19:]
+	count = count + 1
+	sum = sum + float(fy)
+print ('Average spam confidence: ',sum/count)
