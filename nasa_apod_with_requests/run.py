be97b906622502935c00b8c1bfa4cbb96ebeb1db
Your Name <you@example.com>
2020-10-08 14:13:33 +0300

    fixed linting

--- nasa_apod_with_requests/run.py
+++ nasa_apod_with_requests/run.py
@@ -14,14 +14,11 @@ print(img_.headers)
 print(img_.headers["content-type"], img_.headers["content-length"])
 content_type = img_.headers["content-type"]
 
-if (
-    img_.status_code == 200 and
-    (content_type == "image/jpeg" or content_type == "image/gif" or content_type == "image/png")
-):
-    ext = img_.headers["content-type"][6:]
-    if (not os.path.exists ("img/")):
-        os.mkdir("img/")
-    path = f"img/apod_{date}.{ext}"
-    with open(path, "wb") as f:
-        for chunk in img_:
-            f.write(chunk)
+if (img_.status_code == 200 and (content_type == "image/jpeg" or content_type == "image/gif" or content_type == "image/png")):
+	ext = img_.headers["content-type"][6:]
+	if (not os.path.exists ("img/")):
+		os.mkdir("img/")
+	path = f"img/apod_{date}.{ext}"
+	with open(path, "wb") as f:
+		for chunk in img_:
+			f.write(chunk)
