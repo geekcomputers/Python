688d7e11e47dc26ecbc1eb6ff41375d5d6672faf
sandeepdixit0401 <72873808+sandeepdixit0401@users.noreply.github.com>
2020-10-16 05:35:25 -1200

    fact.py
    find factorial of given no.

---
+++ fact.py
@@ -0,0 +1,15 @@
+
+num = 7
+
+
+
+factorial = 1
+
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
