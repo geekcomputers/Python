32ae1f57eec7d8913da7dd0b849d10040fdf65a3
THBESTPROGRAMMER <38732580+THBESTPROGRAMMER@users.noreply.github.com>
2020-10-31 22:54:35 +0530

    Added a file for issues #1088

---
+++ CheckForEligibleToVoteOrNot.py
@@ -0,0 +1,10 @@
+input_age = int(input())
+#TAKING USER AGE
+
+
+#CHECKING CONDITION FOR EDIBLIE TO VOTE
+
+if input_age >= 18:
+    print("You can Vote..")
+else:
+    print("You cannot Vote..")          
