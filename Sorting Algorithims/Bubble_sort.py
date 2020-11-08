120aa6692359cf051da041c4e6557ceb9f394dfe
Zeaan <59172904+Zeaan@users.noreply.github.com>
2020-11-08 14:58:20 +0530

    Update Bubble_sort.py
    Added a code that lets the user create and enter the values of the array and verify by himself/herself.

--- Sorting Algorithims/Bubble_sort.py
+++ Sorting Algorithims/Bubble_sort.py
@@ -13,3 +13,13 @@ if __name__=='__main__':
     list_ = [5, 2, 1, 8, 4]
     bubble_sort(list_)
     print(list_)
+    
+#Lets the user enter values of an array and verify by himself/herself
+array = []
+array_length = int(input(print("Enter the number of elements of array or enter the length of array")))
+for i in range(array_length):
+    value = int(input(print("Enter the value in the array")))
+    array.append(value)
+    
+bubble_sort(array)    
+print(array)
