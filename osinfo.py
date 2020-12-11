0d9ead27ca019c7cc5262df323c81b3d97813983
piterson05 <75846604+piterson05@users.noreply.github.com>
2020-12-11 23:43:18 +0100

    Update osinfo.py
    Added colorama, from now colors will work in Windows cmd

--- osinfo.py
+++ osinfo.py
@@ -1,17 +1,19 @@
 # Script Name		: osinfo.py
 # Authors		: {'geekcomputers': 'Craig Richards', 'dmahugh': 'Doug Mahugh','rutvik1010':'Rutvik Narayana Nadimpally','y12uc231': 'Satyapriya Krishna', 'minto4644':'Mohit Kumar'}
 # Created		: 5th April 2012
-# Last Modified	        : July 19 2016
+# Last Modified	        : December 11 2020
 # Version		: 1.0
 
 # Modification 1	: Changed the profile to list again. Order is important. Everytime we run script we don't want to see different ordering.
 # Modification 2        : Fixed the AttributeError checking for all properties. Using hasttr().
 # Modification 3        : Removed ': ' from properties inside profile.
+# Modification 4        : Added colorama, from now colors will work in Windows cmd
 
 
 # Description		: Displays some information about the OS you are running this script on
 
 import platform as pl
+import colorama
 
 profile = [
     'architecture',
@@ -41,7 +43,8 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
 
-
+colorama.init()
 for key in profile:
     if hasattr(pl, key):
         print(key + bcolors.BOLD + ": " + str(getattr(pl, key)()) + bcolors.ENDC)
+colorama.deinit()
