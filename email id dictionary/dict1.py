a57f9cd00265c7a4259e6e5efb2da4e44d9380fb
Navya <43726071+gedelanavya@users.noreply.github.com>
2021-04-16 16:08:34 +0530

    Update dict1.py

--- email id dictionary/dict1.py
+++ email id dictionary/dict1.py
@@ -13,8 +13,8 @@ for line in fh:
     mail=id[1]
     mails.append(mail)
 
-freq_mail = max(mails, key=mails.count)  #To find frequent mail
-print(freq_mail,mails.count(freq_mail))  #To find countof frequent mail 
+freq_mail = max(mails, key=mails.count)  #to find frequent mail
+print(freq_mail,mails.count(freq_mail))  #to find countof frequent mail 
 
 
 
