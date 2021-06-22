0e778a538201178ed96cf27bbb549a2386ed79ba
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-06-22 19:33:49 +0530

    Create consonant.py
    counts the number of consonant in given string.

---
+++ consonant.py
@@ -0,0 +1,12 @@
+my_string=input("Enter a string to count number of consonants: ")
+string_check=["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]   #list for checking vowels
+
+def count_con(string):
+  c=0
+  for i in range(len(string)):
+    if string[i] not in string_check:  #counter increases if the character is not vowel
+      c += 1
+  return c   
+
+counter = count_con(my_string)
+print(f"Number of consonants in {my_string} is {counter}.")
