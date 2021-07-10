7ef35e39f681db251bd64b85fe8c7a910cfd5608
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-07-10 18:57:22 +0530

    Update area_of_square.py

--- area_of_square.py
+++ area_of_square.py
@@ -1,7 +1,9 @@
 # Returns the area of the square with given sides
 # Side length should eb given in input
 
-side = float(input("Enter the side of the square: "))
+side = 2.4
+
+# side = float(input("Enter the side of the square: "))
 area = round(side*side)
 
 print(f"Area of the given square is {area}.")
