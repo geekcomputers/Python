266a6c6fb9470e141164b471016d6909eaf49b20
Ashish <68690233+AsheeshhSenpai@users.noreply.github.com>
2021-07-10 18:54:29 +0530

    Create area_of_square.py
    program to find area of square

---
+++ area_of_square.py
@@ -0,0 +1,7 @@
+# Returns the area of the square with given sides
+# Side length should eb given in input
+
+side = float(input("Enter the side of the square: "))
+area = round(side*side)
+
+print(f"Area of the given square is {area}.")
