cfc88d91758b6982a1ae475fc7dc8273f43fb6c8
geekboi <85869106+geekboi777@users.noreply.github.com>
2021-07-24 17:34:41 +0530

    Create Fibonacci_sequence_recursive_sol.py

---
+++ Fibonacci_sequence_recursive_sol.py
@@ -0,0 +1,10 @@
+def fib(term):
+   if term <= 1:
+       return (term)
+   else:
+       return (fib(term-1) + fib(term-2))
+
+# Change this value to adjust the number of terms in the sequence.
+number_of_terms = int(input())
+for i in range(number_of_terms):
+    print(fib(i))
