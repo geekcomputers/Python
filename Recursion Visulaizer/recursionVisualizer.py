c01f7ac9fa30d2431b9211b17543adb4cd303aba
Sunder <anand.sunder@capgemini.com>
2021-09-17 16:31:42 +0530

    Mag field

--- Recursion Visulaizer/recursionVisualizer.py
+++ Recursion Visulaizer/recursionVisualizer.py
@@ -1,7 +1,7 @@
 import turtle
 t = turtle.Turtle()
 t.left(90)
-t.speed(200)
+t.speed(100)
 
 def tree(i):
     if i<10:
@@ -11,22 +11,16 @@ def tree(i):
         t.left(10)
         t.right(5)
         t.left(2.5)
-        t.backward(10)
+        t.backward(20)
         tree(3*i/4)
         t.right(20)
         tree(3*i/4)
         t.left(10)
         t.right(5)
         t.left(2.5)
-        t.forward(10)
+        t.forward(15)
         t.backward(i)
-
 tree(40)
-tree(35)
 tree(30)
-tree(25)
-tree(20)
-tree(25)
-tree(35)
 tree(40)
 turtle.done()
