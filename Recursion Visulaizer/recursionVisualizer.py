72f74e8ed96f785335e3b90f73f3ca88a1814d40
Sunder <anand.sunder@capgemini.com>
2021-09-18 19:14:18 +0530

    Try catch block for exception handling

--- Recursion Visulaizer/recursionVisualizer.py
+++ Recursion Visulaizer/recursionVisualizer.py
@@ -8,8 +8,8 @@ def tree(i):
     if i<10:
         return
     else:
-        t.forward(i)
-        t.backward(i-2)
+        t.forward(random.randint(1,i))
+        t.backward(random.randint(1,i-2))
         t.left(100)
         t.right(50)
         t.left(25)
@@ -24,28 +24,35 @@ def tree(i):
         t.forward(15)
         t.right(50)
         t.left(100)
-        t.backward(i-1)
-        t.forward(i+2)
+        t.backward(random.randint(1,i-1))
+        t.forward(random.randint(1,i+2))
+        print('tree execution complete')
 
 def cycle(i):
     if i<100:
         return 
     else:
-        tree(i/5)
-        tree(i/4)
-        tree(i/3)
-        tree(i/2)
-        tree(i)
-        tree(i*1.5)
+        try:
+          tree(i/5)
+          tree(i/4)
+          tree(i/3)
+          tree(i/2)
+          tree(i)
+          tree(i*1.5)
+        except:
+          print('Integer Exception')
+        print('cycle loop complete')
 
 def fractal(i):
     if i<100:
         return
     else:
-         cycle(random.randint(0,i+1))
-         cycle(random.randint(0,i))
-         cycle(random.randint(0,i-1))
-         cycle(random.randint(0,i-2))
+         cycle(random.randint(1,i+1))
+         cycle(random.randint(1,i))
+         cycle(random.randint(1,i-1))
+         cycle(random.randint(1,i-2))
+         print('fractal execution complete')
 
-fractal(200)
+fractal(random.randint(1,200))
+print('Execution complete')
 turtle.done()
