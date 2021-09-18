e3eb3ac892f63139ff092e94ae2abb6ec5f55ce9
Sunder <anand.sunder@capgemini.com>
2021-09-18 17:33:08 +0530

    from birds of prey to birds that swim

--- Recursion Visulaizer/recursionVisualizer.py
+++ Recursion Visulaizer/recursionVisualizer.py
@@ -30,25 +30,21 @@ def cycle(i):
     if i<100:
         return 
     else:
-        tree(50)
-        tree(40)
-        tree(35)
-        tree(5)
-        tree(35)
-        tree(40)
-        tree(50)
-
+        tree(i/5)
+        tree(i/4)
+        tree(i/3)
+        tree(i/2)
+        tree(i)
+        tree(i*1.5)
 
 def fractal(i):
     if i<100:
         return
     else:
          cycle(i+1)
-         tree(i-1)
+         cycle(i)
+         cycle(i-1)
          cycle(i-2)
-         tree(i-2)
-         tree(i-1)
-         cycle(i+1)
 
-fractal(200000)
+fractal(2000)
 turtle.done()
