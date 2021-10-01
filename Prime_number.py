4be2839f3f12a048e1c45252e417505a94448537
henry-the-vietnamese <henryfromvietnam@gmail.com>
2021-10-01 12:36:40 +0700

    three other ways to check prime number

--- Prime_number.py
+++ Prime_number.py
@@ -1,18 +1,40 @@
-import math
-while True:
-    try:
-        num = int(input("Enter a Number: "))
-        break
-    except ValueError:
-        print("Invalid Input")
+# Author:       Tan Duc Mai
+# Email:        tan.duc.work@gmail.com
+# Description:  Three different functions to check whether a given number is a prime.
+#               Return True if it is a prime, False otherwise.
+#               Those three functions, from a to c, decreases in efficiency
+#               (takes longer time).
 
-if num > 1:
-    for i in range(2,int(math.sqrt(num))):  #Smallest Prime Factor of a Composite Number is less than or equal to Square Root of N
-        if (num % i) == 0:
-            print(num,"is NOT a Prime Number. It's indeed a COMPOSITE NUMBER")
-            break
-    else:
-        print(num,"is a PRIME NUMBER ")
-     
-else:
-    print(num,"is NOT a Prime Number")
+from math import sqrt
+
+
+def is_prime_a(n):
+    if n < 2:
+        return False
+    sqrt_n = int(sqrt(n))
+    for i in range(2, sqrt_n + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+def is_prime_b(n):
+    if n > 1:
+        if n == 2:
+            return True
+        else:
+            for i in range(2, n):
+                if n % i == 0:
+                    return False
+            return True
+    return False
+
+
+def is_prime_c(n):
+    divisible = 0
+    for i in range(1, n + 1):
+        if n % i == 0:
+            divisible += 1
+    if divisible == 2:
+        return True
+    return False
