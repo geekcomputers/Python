fa5f487eb24f61bf2c868ab25b0d16c60f34896b
Samarth Dhir <samarthdhir57.2014@gmail.com>
2021-10-20 23:08:53 +0530

    QR Code Generator and decoder

---
+++ qrdecoder.py
@@ -0,0 +1,10 @@
+#Importing Required Modules
+import cv2
+
+#QR Code Decoder
+
+image = cv2.imread(filename) #Enter name of the image
+detector = cv2.QRCodeDetector()
+data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
+if vertices_array is not None: 
+  print(data)
