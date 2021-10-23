fde19018b8b13a00ff33f374c4ddd4259ee5acca
Rohan R Bharadwaj <89947037+Rohanrbharadwaj@users.noreply.github.com>
2021-10-23 19:23:40 +0530

    Update qrdecoder.py

--- qrdecoder.py
+++ qrdecoder.py
@@ -3,6 +3,7 @@ import cv2
 
 #QR Code Decoder
 
+filename = input()
 image = cv2.imread(filename) #Enter name of the image
 detector = cv2.QRCodeDetector()
 data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
