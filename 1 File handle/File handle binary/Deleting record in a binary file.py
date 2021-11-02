f75e226c99af41da9d0d062fc0a551c6685681a9
Aditya Kumar <93466706+adityakumar-in@users.noreply.github.com>
2021-11-02 09:08:33 +0000

    Added some whitespaces & comments for better understanding!

--- 1 File handle/File handle binary/Deleting record in a binary file.py
+++ 1 File handle/File handle binary/Deleting record in a binary file.py
@@ -1,10 +1,15 @@
 import pickle
+
 def Bdelete():
+    # Opening a file & loading it
     F= open("studrec.dat","rb")
     stud = pickle.load(F)
     F.close()
+    
     print(stud)
-    rno= int(input("Enter the roll number to be deleted"))
+    
+    # Deleting the Roll no. entered by user
+    rno= int(input("Enter the Roll no. to be deleted: "))
     F= open("studrec.dat","wb")
     rec= []
     for i in stud:
@@ -13,4 +18,5 @@ def Bdelete():
         rec.append(i)
     pickle.dump(rec,F)
     F.close()
+    
 Bdelete()
