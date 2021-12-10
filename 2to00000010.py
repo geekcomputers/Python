902074705ab7e604f217b6219a5effe7ada67929
Jym Patel <93824505+JymPatel@users.noreply.github.com>
2021-12-10 23:53:02 +0530

    Update 2to00000010.py

--- 2to00000010.py
+++ 2to00000010.py
@@ -6,15 +6,8 @@ import sys
 binary = '$' # just starting var
 n = 15 # can get 2**16 numbers
 
-# get integer as output which is less than limit 2**16
-try: 
-    input = int(input("What is your Decimal Number?"))
-    limit = 2**(n + 1)
-    input <= limit
-except ValueError:
-    print("Please put integer in input! & less than", limit)
-    sys.exit()
 
+input = int(input("What is your Decimal Num
 # main algorithm
 while n >= 0:
     if input < 2**n:
