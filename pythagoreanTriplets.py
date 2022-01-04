65d2e1922114fbccf36f6b4b17c8fb8a47d2c39e
Rituraj Mahato <94545894+Rituraj-M50@users.noreply.github.com>
2022-01-04 08:59:39 +0530

    Create pythagoreanTriplets.py

---
+++ pythagoreanTriplets.py
@@ -0,0 +1,14 @@
+limit=int(input("Enter upper limit:"))
+c=0
+m=2
+while(c<limit):
+    for n in range(1,m+1):
+        a=m*m-n*n
+        b=2*m*n
+        c=m*m+n*n
+        if(c>limit):
+            break
+        if(a==0 or b==0 or c==0):
+            break
+        print(a,b,c)
+    m=m+1
