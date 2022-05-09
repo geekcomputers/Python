5d33a509012e56dc50935f55079f84df626f7927
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-05-09 10:25:50 +0530

    improved the way of file handling

--- 1 File handle/File handle text/question3.py
+++ 1 File handle/File handle text/question3.py
@@ -4,33 +4,29 @@ the number of lines which starts with either “I‟ or “M‟."""
 
 # step1:
 def write_to_file():
-    F = open("happy.txt", "a")
-    while True:
-        text = input("enter any text")
-
-        F.write(
-            text + "\n"
-        )  # write function takes exactly 1 arguement so concatenation
-        choice = input("do you want to enter more, y/n")
-        if choice == "n":
-            break
-    F.close()
-
-
+    with open("happy.txt", "a") as F:
+        while True:
+            text = input("enter any text")
+            F.write(
+                text + "\n"
+            )  # write function takes exactly 1 arguement so concatenation
+            choice = input("do you want to enter more, y/n")
+            if choice == "n":
+                break
+        
 # write_to_file()
 
 # step2:
 def check_first_letter():
-    F = open("happy.txt", "r")
-    value = F.read()
-    count = 0
-    line = value.split()
-    for i in line:
-        if i[0] in ["m", "M", "i", "I"]:
-            count += 1
-            print(i)
+    with open("happy.txt") as F:
+        value = F.read()
+        count = 0
+        line = value.split()
+        for i in line:
+            if i[0] in ["m", "M", "i", "I"]:
+                count += 1
+                print(i)
+        print("The total number of sentences starting with I or M are", count)
 
-    print("The total number of sentences starting with I or M are", count)
-
-
-check_first_letter()
+if __name__ == "__main__":
+    check_first_letter()
