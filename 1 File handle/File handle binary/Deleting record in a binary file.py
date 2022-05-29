951948c9f5b137ecf15790d92ae06af69c44662a
Kristopher Roller <akjroller@gmail.com>
2022-05-28 23:06:08 -0400

    fixed naming convention
    Fixed naming convention for adhere to PIP-8

--- 1 File handle/File handle binary/Deleting record in a binary file.py
+++ 1 File handle/File handle binary/Deleting record in a binary file.py
@@ -1,24 +1,21 @@
 import pickle
 
 
-def Bdelete():
+def bdelete():
     # Opening a file & loading it
-    F = open("studrec.dat", "rb")
-    stud = pickle.load(F)
-    F.close()
-
-    print(stud)
+    with open("studrec.dat") as F:
+        stud = pickle.load(F)
+        print(stud)
 
     # Deleting the Roll no. entered by user
     rno = int(input("Enter the Roll no. to be deleted: "))
-    F = open("studrec.dat", "wb")
-    rec = []
-    for i in stud:
-        if i[0] == rno:
-            continue
-        rec.append(i)
-    pickle.dump(rec, F)
-    F.close()
+    with open("studrec.dat") as F:
+        rec = []
+        for i in stud:
+            if i[0] == rno:
+                continue
+            rec.append(i)
+        pickle.dump(rec, F)
 
 
-Bdelete()
+bdelete()
