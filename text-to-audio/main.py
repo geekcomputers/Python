0de1be509318fd42438253f19f0b9f329e3af77f
Vardhaman <83634399+cyai@users.noreply.github.com>
2022-06-09 11:23:12 +0530

    Update main.py

--- text-to-audio/main.py
+++ text-to-audio/main.py
@@ -7,6 +7,15 @@ mytext = "Hello World!, this audio is created using GTTS module."
 # Specify the language in which you want your audio
 language = "en"
 
+
+# To convert the txt file into audio, uncomment the below lines.
+# Change the file name with the name of your txt file below:
+#myfile = 'name_of_your_txt_file.txt'
+#with open(myfile, 'r') as f:
+#    text_from_file = f.read()
+#    f.close()
+#myobj = gTTS(text=text_from_file, lang = language, slow=False)
+
 # Create an instance of gTTS class
 myobj = gTTS(text=mytext, lang=language, slow=False)
 
@@ -15,4 +24,5 @@ myobj.save("hello_world.mp3")
 print("Audio Saved")
 
 # This will play your audio file
-os.system("mpg321 welcome.mp3")
+os.system("mpg321 hello_world.mp3")
+
