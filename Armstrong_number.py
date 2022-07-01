0689f84d3883c4e718684b282d05864e4d182712
unknown <arthitapaul958@gmail.com>
2022-07-01 19:15:14 +0530

    Armstrong_number-more-simplification

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -4,8 +4,9 @@ n = int(a)
 S = 0
 while n > 0:
     d = n % 10
-    S = S + d * d * d
-    n = n / 10
+    S = S + (d**3)
+    # There was an error here, it is not n = n / 10, it is n = n // 10
+    n = n // 10
 if int(a) == S:
     print("Armstrong Number")
 else:
