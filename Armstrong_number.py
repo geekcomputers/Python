8a6ee233181a3b6616a7d8ba4f03eb1850a0847f
Aliwahid17 <88263575+Aliwahid17@users.noreply.github.com>
2022-07-02 18:49:29 +0530

    make it clean and fast

--- Armstrong_number.py
+++ Armstrong_number.py
@@ -1,12 +1,13 @@
-# checking for armstrong number
-a = input("Enter a number")
-n = int(a)
-S = 0
-while n > 0:
-    d = n % 10
-    S = S + d * d * d
-    n = n / 10
-if int(a) == S:
-    print("Armstrong Number")
-else:
-    print("Not an Armstrong Number")
+def is_armstrong_number(number):
+    numberstr = str(number)
+    length = len(numberstr)
+    num = number
+    rev = 0
+    temp = 0
+    while num != 0:
+        rem = num % 10
+        num //=  10
+        temp += rem ** length
+    return temp == number
+
+is_armstrong_number(number)
