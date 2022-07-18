5cfc41c0d33d9c07f19e39816cba2de8f6de0be6
Harivansh Thakur <harivanshthakur8@gmail.com>
2022-07-18 21:58:53 +0530

    fixed the indentation issue

--- Grocery calculator.py
+++ Grocery calculator.py
@@ -9,28 +9,27 @@ take it to the store!'''
 #Object = GroceryList
 #Methods = addToList, Total, Subtotal, returnList
 
+
 class GroceryList(dict):
+    def __init__(self): self = {}
 
-    def __init__(self):
-    	self = {}
-
-    def addToList(self, item, price):
-            self.update({item:price})
+    def addToList(self, item, price): self.update({item: price})
 
     def Total(self):
-		total = 0
-		for items in self:
-			total += (self[items])*.07 + (self[items])
-		return total
+        total = 0
+        for items in self:
+            total += (self[items])*.07 + (self[items])
+        return total
 
     def Subtotal(self):
-		subtotal = 0
-		for items in self:
-			subtotal += self[items]
-		return subtotal
+        subtotal = 0
+        for items in self:
+            subtotal += self[items]
+        return subtotal
 
     def returnList(self):
-		return self
+        return self
+
 
 '''Test list should return:
 Total = 10.70
@@ -39,18 +38,18 @@ returnList = {"milk":4, "eggs":3, "kombucha":3}
 '''
 List1 = GroceryList()
 
-List1.addToList("milk",4)
+List1.addToList("milk", 4)
 List1.addToList("eggs", 3)
 List1.addToList("kombucha", 3)
 
 
-print List1.Total()
-print List1.Subtotal()
-print List1.returnList()
+print(List1.Total())
+print(List1.Subtotal())
+print(List1.returnList())
 
-#*****************************************************
+# *****************************************************
 print
-#*****************************************************
+# *****************************************************
 
 
 List2 = GroceryList()
@@ -59,6 +58,6 @@ List2.addToList('cheese', 7.49)
 List2.addToList('wine', 25.36)
 List2.addToList('steak', 17.64)
 
-print List2.Total()
-print List2.Subtotal()
-print List2.returnList()
+print(List2.Total())
+print(List2.Subtotal())
+print(List2.returnList())
