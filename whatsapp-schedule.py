0399eb69fd0ddd17c6dc321dd05952191d9f6b1b
rein14 <reinrich14@gmail.com>
2022-09-27 13:44:42 +0000

    Update whatsapp-schedule.py
    changes to whatsapp scheduler

--- whatsapp-schedule.py
+++ whatsapp-schedule.py
@@ -1,9 +1,9 @@
 """
-Author - Richmond Nyamekye
+Author- Richmond Nyamekye 
 
-install requirements: python -m pip install pywhatkit
+download https://github.com/mozilla/geckodriver/releases
 
-Schedule a whatsapp message to be sent at your convenience
+install requirements: python -m pip install selenium
 
 """
 import pywhatkit
@@ -14,14 +14,13 @@ def send_msg(phone: str, msg: str, hour: int, minute: int) -> None:
 
 
 def send_whatmsg_to_group(group: str, msg: str, hour: int, minute: int) -> None:
-    pywhatkit.sendwhatmsg_to_group(group, msg, hour, minute)
+    pywhatkit.send_whatmsg_to_group(group, msg, hour, minute)
 
 
 def main():
     msg_type = int(input("Enter 1 to send a message to a uSER and 2 to a GROUP: "))
     if msg_type == 1:
         phone = input("Enter phone number: ")
-        #make changes to suit your needs
         if phone[0] == "0":
             phone = phone[1::]
         while True:
