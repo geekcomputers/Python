a18a6ca03979239dfbcde8e28716a2987054b7c0
NIKITA PANDEY <113332472+nikitapandeyy@users.noreply.github.com>
2023-03-16 10:23:24 +0530

    Update youtubedownloader.py
    The use of global variables should be minimized as much as possible. In the current code, url_box is a global variable. Instead, you can pass the url_box as a parameter to the download function.
    It is good practice to include exception handling in your code, especially when working with web requests. You can add a try-except block around the line url = YouTube(str(url_box.get())) to handle any errors that may occur.
    You can add a progress bar to show the progress of the download. This will give the user a better idea of how much time is left until the download is complete.
    You can also add a feature to let the user choose the download location instead of downloading to the default directory.

--- youtubedownloader.py
+++ youtubedownloader.py
@@ -1,26 +1,30 @@
 from tkinter import *
+from tkinter import filedialog, messagebox
 from threading import Thread
-from tkinter import messagebox
 from pytube import YouTube
 
 
 def threading():
     # Call work function
-    t1=Thread(target=download)
+    t1 = Thread(target=download)
     t1.start()
 
 def download():
-    url = YouTube(str(url_box.get()))
-    video = url.streams.first()
-    video.download()    
-    messagebox.showinfo('', 'Download completed!')
+    try:
+        url = YouTube(str(url_box.get()))
+        video = url.streams.first()
+        filename = filedialog.asksaveasfilename(defaultextension=".mp4", filetypes=[("MP4 files", "*.mp4")])
+        video.download(filename=filename)
+        messagebox.showinfo('', 'Download completed!')
+    except Exception as e:
+        messagebox.showerror("Error", "An error occurred while downloading the video.")
 
-root =Tk()
+
+root = Tk()
 root.title('YouTube Downloader')
 root.geometry('780x500+200+200')
-#root.iconbitmap('youtube.ico')
 root.configure(bg='olivedrab1')
-root.resizable(False,False)
+root.resizable(False, False)
 
 # Label widgets
 introlable = Label(
@@ -29,9 +33,9 @@ introlable = Label(
     width=30,
     relief='ridge',
     bd=4,
-    font=('chiller',26,'italic bold'),
+    font=('chiller', 26, 'italic bold'),
     fg='red')
-introlable.place(x=35,y=20)
+introlable.place(x=35, y=20)
 
 Label(
     root, 
@@ -40,55 +44,13 @@ Label(
     bg='olivedrab1'
     ).place(x=40, y=150)
 
-# DownloadingSizeLabel = Label(
-#     root,
-#     text='Total Size: ',
-#     font=('arial',15,'italic bold'),
-#     bg='olivedrab1'
-#     )
-# DownloadingSizeLabel.place(x=500,y=240)
-
-# DownloadingLabel = Label(
-#     root,
-#     text='Recieved Size: ',
-#     font=('arial',15,'italic bold'),
-#     bg='olivedrab1'
-#     )
-# DownloadingLabel.place(x=500,y=290)
-
-# DownloadingTime = Label(
-#     root,
-#     text='Time Left : ',
-#     font=('arial',15,'italic bold'),
-#     bg='olivedrab1'
-#     )
-# DownloadingTime.place(x=500,y=340)
-
-# DownloadingSizeLabelResult = Label(
-#     root,
-#     text=' ',
-#     font=('arial',15,'italic bold'),
-#     bg='olivedrab1'
-#     )
-# DownloadingSizeLabelResult.place(x=650,y=240)
-
-# DownloadingLabelResult = Label(
-#     root,
-#     text=' ',
-#     font=('arial',15,'italic bold'),
-#     bg='olivedrab1'
-#     )
-# DownloadingLabelResult.place(x=650,y=290)
-
-# Entry widgets
 url_box = Entry(
     root, 
-    font=('arial',30), 
+    font=('arial', 30), 
     width=30
     )
 url_box.place(x=40, y=180)
 
-# Button Widgets
 btn = Button(
     root, 
     text='DOWNLOAD', 
