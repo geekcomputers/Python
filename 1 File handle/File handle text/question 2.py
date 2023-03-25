37450fc22c5629d66f7ad5e85d095782fa71da51
muzakkirsaifi123 <mohd.saifi@knoldus.com>
2023-03-25 23:55:33 +0530

    make user interactive

--- 1 File handle/File handle text/question 2.py
+++ 1 File handle/File handle text/question 2.py
@@ -4,16 +4,19 @@
 and display those words, which are less than 4 characters. """
 
 def display_words():
-    with open("story.txt") as F:
-        lines = F.read()
-        words = lines.split()
-        count = 0
-        for word in words:
-            if (len(word) < 4):
-                print(word)
-                count += 1
-    return count 
-
+    try:
+        with open("story.txt") as F:
+            lines = F.read()
+            words = lines.split()
+            count = 0
+            for word in words:
+                if (len(word) < 4):
+                    print(word)
+                    count += 1
+        return count 
+    except FileNotFoundError:
+        print("File not found")
+        
 if __name__ == "__main__":
     print(display_words())
                 
