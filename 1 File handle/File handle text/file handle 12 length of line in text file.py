9e3b653d094b8fe716f0dd6c77f6d4f7344a88f5
muzakkirsaifi123 <mohd.saifi@knoldus.com>
2023-03-31 23:59:42 +0530

    update in code

--- 1 File handle/File handle text/file handle 12 length of line in text file.py
+++ 1 File handle/File handle text/file handle 12 length of line in text file.py
@@ -1,10 +1,38 @@
+
+import os
+import time
+file_name= input("Enter the file name to create:- ")
+
+print(file_name)
+
+def write_to_file(file_name):
+
+    if os.path.exists(file_name):
+        print(f"Error: {file_name} already exists.")
+
+    else:
+        with open(file_name, "a") as F:
+            while True:
+                text = input("enter any text to add in the file:- ")
+                F.write(
+                    text + "\n"
+                )  # write function takes exactly 1 arguement so concatenation
+                choice = input("Do you want to enter more, y/n")
+                if choice == "n":
+                    break
+        
 def longlines():
-    with open('story.txt', encoding='utf-8') as F:
+    with open(file_name, encoding='utf-8') as F:
         lines = F.readlines()
 
         for i in lines:
             if len(i) < 50:
                 print(i, end="\t")
+            else: 
+                print("There is no line which is less than 50 ")
 
 
-longlines()
+if __name__ == "__main__":
+    write_to_file(file_name)
+    time.sleep(1)
+    longlines()
