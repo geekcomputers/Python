c8ca27ba2bca8810afa86d82f9e3d8c5e71b986d
y8l <72172663+IMperiumX@users.noreply.github.com>
2023-05-18 00:56:37 +0300

    chore: fixed missed up commas with print statement

--- Add two numbers.py
+++ Add two numbers.py
@@ -1,10 +1,10 @@
 # User pick two numbers to sum
 
-num1 = float(input("Number 1:"))
-num2 = float(input("Number 2:"))
+num1 = float(input("Number 1: "))
+num2 = float(input("Number 2: "))
 
 # Add two numbers
 sum = num1 + num2
 
 # Display the sum
-print("The sum of"num1,'and'num2,'is',sum)
+print("The sum of",num1,'and2',num2,'is',sum)
