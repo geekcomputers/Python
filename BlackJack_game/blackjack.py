1832da8211a1e00a1ee55894543eb9d29d893be6
YoHomeE <136176988+YoHomeE@users.noreply.github.com>
2023-06-10 16:34:25 +0200

    Update blackjack.py
    Ammended the input character "1" to a string right at the bottom

--- BlackJack_game/blackjack.py
+++ BlackJack_game/blackjack.py
@@ -107,7 +107,7 @@ while sum(p_cards) < 21:
 
     # to continue the game again and again !!
     k = input("Want to hit or stay?\n Press 1 for hit and 0 for stay ")
-    if k == 1:
+    if k == "1": #Ammended 1 to a string
         random.shuffle(deck)
         p_cards.append(deck.pop())
         print("You have a total of " + str(sum(p_cards)) + " with the cards ", p_cards)
