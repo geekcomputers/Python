5d9055666abbe33030d72c537be1d1fce8bc250b
Andrea Piombo <piombacciaio@gmail.com>
2023-07-30 13:00:12 +0200

    Update Grocery calculator.py
    Fixed inconsistent use of tabs and spaces in indentation
    Updated print statements to match current syntax

--- Grocery calculator.py
+++ Grocery calculator.py
@@ -8,28 +8,28 @@ take it to the store!'''
 
 #Object = GroceryList
 #Methods = addToList, Total, Subtotal, returnList
-
 class GroceryList(dict):
 
-    def __init__(self):
-    	self = {}
+	def __init__(self):
+		self = {}
 
-    def addToList(self, item, price):
-            self.update({item:price})
+	def addToList(self, item, price):
+        
+		self.update({item:price})
 
-    def Total(self):
+	def Total(self):
 		total = 0
 		for items in self:
 			total += (self[items])*.07 + (self[items])
 		return total
 
-    def Subtotal(self):
+	def Subtotal(self):
 		subtotal = 0
 		for items in self:
 			subtotal += self[items]
 		return subtotal
 
-    def returnList(self):
+	def returnList(self):
 		return self
 
 '''Test list should return:
@@ -44,12 +44,12 @@ List1.addToList("eggs", 3)
 List1.addToList("kombucha", 3)
 
 
-print List1.Total()
-print List1.Subtotal()
-print List1.returnList()
+print(List1.Total())
+print(List1.Subtotal())
+print(List1.returnList())
 
 #*****************************************************
-print
+print()
 #*****************************************************
 
 
@@ -59,6 +59,6 @@ List2.addToList('cheese', 7.49)
 List2.addToList('wine', 25.36)
 List2.addToList('steak', 17.64)
 
-print List2.Total()
-print List2.Subtotal()
-print List2.returnList()
+print(List2.Total())
+print(List2.Subtotal())
+print(List2.returnList())
