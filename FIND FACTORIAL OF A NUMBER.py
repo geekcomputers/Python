a71326458fd7e452d03f2e5feb378c7e14585270
sarayu sree <sarayusreeyadavpadala@gmail.com>
2023-08-10 22:39:52 +0530

    few comments are added

--- FIND FACTORIAL OF A NUMBER.py
+++ FIND FACTORIAL OF A NUMBER.py
@@ -1,12 +1,13 @@
 # Python program to find the factorial of a number provided by the user.
 
 def factorial(n):
-	if n < 0:
+	if n < 0:         # factorial of number less than 0 is not possible
 		return "Oops!Factorial Not Possible"
-	elif n == 0:
+	elif n == 0:    # 0! = 1; when n=0 it returns 1 to the function which is calling it previously. 
 		return 1
 	else:
-		return n*factorial(n-1)
+		return n*factorial(n-1)  
+#Recursive function. At every iteration "n" is getting reduced by 1 until the "n" is equal to 0.
 
-n = int(input())
-print(factorial(n))
+n = int(input("Enter a number: ")) # asks the user for input 
+print(factorial(n))    # function call
