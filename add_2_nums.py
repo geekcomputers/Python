27b792f74c41e9cfb2d81ff3e20bbd98b14ad99a
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-08-10 13:55:17 +0530

    Duplicate file as of previous commited, pollutes the repository, serves no extra function, program to serve the same function already present.

--- add_2_nums.py
+++
@@ -1,5 +0,0 @@
-# for item in # this python program asks uses for 2 number and returns their sum:
-
-a = int(input("enter first Number: "))
-b = int(input("enter second Number: "))
-print("sum is:", a+b)
