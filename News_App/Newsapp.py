77b464c6fb859b6f5add91f05b9b8a95e7392591
Rahul Chauhan <113838908+Rjchauhan18@users.noreply.github.com>
2023-10-23 11:10:57 +0530

    Update Newsapp.py

--- News_App/Newsapp.py
+++ News_App/Newsapp.py
@@ -12,12 +12,10 @@ end_date = date.today() + timedelta(days=1)
 
 def News(symbol):
     get_Data = yf.Ticker(symbol)
-    # msft.news
-
+    
     #news section 
     try:
         NEWS = get_Data.news
-        # sr.Markdown(f"{NEWS}")
         sr.Markdown(f"# News of {v.value} :")
         for i in range(len(NEWS)):
             sr.Markdown("\n********************************\n")
@@ -36,7 +34,7 @@ def News(symbol):
     except Exception as e:
         sr.Markdown(e)
         sr.Markdown("No news available")
-# News(select)
+
 
 
 
@@ -48,26 +46,12 @@ def Page():
     with sr.Column() as main:
         with sr.Sidebar():
             sr.Markdown("## **stock Analysis**")
-            # sr.SliderInt(label="Ideal for placing controls")
-            # sr.header("**srock Analysis**")
             sr.Select("Select stock",value=v,values=company)
 
             select=Company_Name.get(v.value)
 
 
-            # sr.Text(select_company)
-        # sr.Info("I'm in the main content area, put your main content here")
-
         News(select)
 
-        # sr.FigurePlotly(qs.plots.daily_returns(ITC,benchmark="US"))
     return main
 
-
-
-# @app.route("/")
-# def hello_world():
-#     return "<p>Hello, World!</p>"
-
-# if __name__=="__main__":
-#     app.run(debug=False)
