ed7f3b0d2a6eca33acec293817d25b83dd6fe944
AndrewB50 <97339555+AndrewB50@users.noreply.github.com>
2023-12-09 02:14:05 -0600

    Update Python Program to Sort Words in Alphabetic Order.py
    The .sort() function sorts strings by ASCII code. With a mix of capitalized and lower case words, they did not return in alphabetical order. I used the .lower() function to ensure the all words returned alphabetically.

--- Python Program to Sort Words in Alphabetic Order.py
+++ Python Program to Sort Words in Alphabetic Order.py
@@ -1,18 +1,16 @@
-# Program to sort alphabetically the words form a string provided by the user
+# Program to sort words alphabetically and put them in a dictionary with corresponding numbered keys
 
+#declaring variables
 my_str = "Hello this Is an Example With cased letters"
+
+#Need to make all words the same case, otherwise, the .sort() function sorts them by ASCII code and they will not appear alphabetically. 
+my_str = my_str.lower()
 
 # To take input from the user
 #my_str = input("Enter a string: ")
 
 # breakdown the string into a list of words
 words = my_str.split()
-
-# sort the list
 words.sort()
 
-# display the sorted words
-
-print("The sorted words are:")
-for word in words:
-   print(word)
+print(words)
