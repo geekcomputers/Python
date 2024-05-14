95de021dd0ab2cb775507f21ae67f0539744f4ba
Xceptions <agbokenechukwu.k@gmail.com>
2024-05-14 23:24:18 +0100

    brought in backend into frontend

--- Search_Engine/frontend.py
+++ Search_Engine/frontend.py
@@ -1,34 +1,37 @@
 from tkinter import *
 from tkinter import messagebox
+import backend
 
 
 def add_document():
     document = add_documents_entry.get()
-    print(document)
+    se = backend.SearchEngine()
+    print(se.index_document(document))
 
 def find_term():
     term = find_term_entry.get()
-    print(term)
+    se = backend.SearchEngine()
+    print(se.find_documents(term))
 
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Registration Form")
+    root.geometry('300x300')
 
-root = Tk()
-root.title("Registration Form")
-root.geometry('300x300')
+    add_documents_label = Label(root, text="Add Document:")
+    add_documents_label.pack()
+    add_documents_entry = Entry(root)
+    add_documents_entry.pack()
 
-add_documents_label = Label(root, text="Add Document:")
-add_documents_label.pack()
-add_documents_entry = Entry(root)
-add_documents_entry.pack()
+    add_document_button = Button(root, text="add", command=add_document)
+    add_document_button.pack()
 
-add_document_button = Button(root, text="add", command=add_document)
-add_document_button.pack()
+    find_term_label = Label(root, text="Input term to search:")
+    find_term_label.pack()
+    find_term_entry = Entry(root)
+    find_term_entry.pack()
 
-find_term_label = Label(root, text="Input term to search:")
-find_term_label.pack()
-find_term_entry = Entry(root)
-find_term_entry.pack()
+    search_term_button = Button(root, text="search", command=find_term)
+    search_term_button.pack()
 
-search_term_button = Button(root, text="search", command=find_term)
-search_term_button.pack()
-
-root.mainloop()
+    root.mainloop()
