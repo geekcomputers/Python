3664b47b0b4361971e2b8042dd5fa9337b728f7f
Franchaisco <f.babila.niba@student.vu.nl>
2024-06-20 12:30:32 +0200

    update

--- power_of_n.py
+++ power_of_n.py
@@ -1,3 +1,5 @@
+import power_of_num_tests as pow
+
 # Assign values to author and version.
 __author__ = "Himanshu Gupta"
 __version__ = "1.0.0"
@@ -21,10 +23,7 @@ def printCov():
             print(branch, "was not hit")
     resetDic()
 
-# def pow_test_one():
-#         result = binaryExponentiation(2.00000, 0)
-#         assert(result == 1)
-#         print("pow-case-1 passed")
+
 
 def binaryExponentiation(x: float, n: int) -> float:
     """
@@ -81,6 +80,6 @@ if __name__ == "__main__":
     print(binaryExponentiation(2.00000, 10))
     print(binaryExponentiation(2.10000, 3))
     print(binaryExponentiation(2.00000, -2))
-    # pow_test_one()
+    pow.pow_test_one()
     
     printCov()
---
+++ power_of_num_tests.py
@@ -0,0 +1,7 @@
+import power_of_n as pow
+
+def pow_test_one():
+        result = pow.binaryExponentiation(2.00000, 0)
+        assert(result == 1)
+        print("pow-case-1 passed")
+
