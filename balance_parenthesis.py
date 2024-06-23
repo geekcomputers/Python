3f9692033e0b0a8a159d8dadd91936c4c6f38e34
Franchaisco <f.babila.niba@student.vu.nl>
2024-06-23 11:59:34 +0200

    update balance_parenthesis

--- balance_parenthesis.py
+++ balance_parenthesis.py
@@ -1,3 +1,10 @@
+coverage = {
+    "bal-case-1" : False,
+    "bal-case-2" : False,
+    "bal-case-3" : False,
+    "bal-case-4" : False
+}
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -36,20 +43,48 @@ def is_balanced(check_string):
     while index < len(check_string) and is_bal:
         paren = check_string[index]
         if paren in "{[(":
+            coverage["bal-case-1"] = True
             s.push(paren)
         else:
             if s.is_empty():
+                coverage["bal-case-2"] = True
                 is_bal = False
             else:
+                coverage["bal-case-3"] = True
                 top = s.pop()
                 if not is_same(top, paren):
+                    coverage["bal-case-4"] = True
                     is_bal = False
         index += 1
 
     if s.is_empty() and is_bal:
+        coverage["bal-case-5"] = True
         return True
+        
     else:
+        coverage["bal-case-6"] = True
         return False
+    
+def bal_case_two_test():
+    empty_str = " "
+    assert(is_balanced(empty_str) == False)
+    print("case-two-test passed OK")
 
+
+def resetDic():
+    for i in coverage.keys():
+        coverage[i] = False
+
+def printCov():
+    for branch, hit in coverage.items():
+        if hit:
+            print(branch, "was hit")
+        else:
+            print(branch, "was not hit")
+    resetDic()
 
 print(is_balanced("[((())})]"))
+bal_case_two_test()
+printCov()
+
+
