b8ff109338f78a41c003dfaf792806fa2824e6cf
Ratnesh Kumar <jnvratneshkumar@gmail.com>
2024-12-13 12:49:47 +0530

    add Mode
    Co-Authored-By: Ratnesh Kumar <142919875+16ratneshkumar@users.noreply.github.com>

--- 1 File handle/File handle binary/Deleting record in a binary file.py
+++ 1 File handle/File handle binary/Deleting record in a binary file.py
@@ -3,13 +3,13 @@ import pickle
 
 def bdelete():
     # Opening a file & loading it
-    with open("studrec.dat") as F:
+    with open("studrec.dat","rb") as F:
         stud = pickle.load(F)
         print(stud)
 
     # Deleting the Roll no. entered by user
     rno = int(input("Enter the Roll no. to be deleted: "))
-    with open("studrec.dat") as F:
+    with open("studrec.dat","wb") as F:
         rec = [i for i in stud if i[0] != rno]
         pickle.dump(rec, F)
 
