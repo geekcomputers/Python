203a9b9ac892ee2d742162594ee805bb2b157550
SomdattaNag <nagsomdatta2@gmail.com>
2025-02-15 22:38:12 +0530

    Added text-to-speech functionality to generate voice from python code(Issue #2511)

---
+++ Python Voice Generator.py
@@ -0,0 +1,11 @@
+#install and import google text-to-speech library gtts
+from gtts import gTTS
+import os
+#provide user input text
+text=input('enter the text: ') 
+#covert text into voice
+voice=gTTS(text=text, lang='en')
+#save the generated voice 
+voice.save('output.mp3')
+#play the file in windows
+os.system('start output.mp3') 
