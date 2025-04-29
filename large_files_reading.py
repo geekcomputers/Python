43b6c1142c7b9a78a3731f123d783597ec8c1be6
Abhilash <abhityson99@gmail.com>
2025-04-29 22:58:13 +0530

    Added python script for reading large files

---
+++ large_files_reading.py
@@ -0,0 +1,4 @@
+with open("new_project.txt", "r" , encoding="utf-8") as file:   # replace "largefile.text" with your actual file name or with absoulte path  
+# encoding = "utf-8" is especially used when the file contains special characters....
+    for f in file:
+        print(f.strip())
