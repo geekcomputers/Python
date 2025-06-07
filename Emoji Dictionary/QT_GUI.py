9e91388d562fc98456eb7d34bae4216ac7b9e6b8
Pratyanj <pratyanjmodh1205@gmail.com>
2025-06-07 11:14:38 +0530

    add emoji show and hide function

--- Emoji Dictionary/QT_GUI.py
+++ Emoji Dictionary/QT_GUI.py
@@ -8,18 +8,24 @@ from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 class MainWindow(QMainWindow):
-    def __init__(self):
+   def __init__(self):
         super(MainWindow, self).__init__()
         
         # Load the UI file
         uic.loadUi('Emoji Dictionary/QT_GUI.ui', self)
         cells = [
-            ["😀", "🥰", "😴", "🤓", "🤮", "🤬", "😨", "🤑", "😫", "😎"],
+            
             ["🐒", "🐕", "🐎", "🐪", "🐁", "🐘", "🦘", "🦈", "🐓", "🐝", "👀", "🦴", "👩🏿", "‍🤝", "🧑", "🏾", "👱🏽", "‍♀", "🎞", "🎨", "⚽"],
             ["🍕", "🍗", "🍜", "☕", "🍴", "🍉", "🍓", "🌴", "🌵", "🛺", "🚲", "🛴", "🚉", "🚀", "✈", "🛰", "🚦", "🏳", "‍🌈", "🌎", "🧭"],
-            ["🔥", "❄", "🌟", "🌞", "🌛", "🌝", "🌧", "🧺", "🧷", "🪒", "⛲", "🗼", "🕌", "👁", "‍🗨", "💬", "™", "💯", "🔕", "💥", "❤"]
+            ["🔥", "❄", "🌟", "🌞", "🌛", "🌝", "🌧", "🧺", "🧷", "🪒", "⛲", "🗼", "🕌", "👁", "‍🗨", "💬", "™", "💯", "🔕", "💥", "❤"],
+            ["😀", "🥰", "😴", "🤓", "🤮", "🤬", "😨", "🤑", "😫", "😎"],
         ]
-        
+        def emoji_wight_btn():
+         if self.emoji_widget.isVisible():
+            self.emoji_widget.hide()
+         else:
+            self.emoji_widget.show() 
+            
         self.emoji_buttons = []
         self.emoji_layout = QGridLayout()
         self.emoji_widget = QWidget()
@@ -27,12 +33,14 @@ class MainWindow(QMainWindow):
         self.frame_2.setLayout(QVBoxLayout())
         self.frame_2.layout().addWidget(self.emoji_widget)
         self.emoji_widget.hide()
-        self.pushButton.clicked.connect(lambda: self.emoji_widget.show())
+        self.pushButton.clicked.connect(lambda:emoji_wight_btn())
+        
         
         for row_idx, row in enumerate(cells):
             for col_idx, emoji in enumerate(row):
                 button = QPushButton(emoji)
                 button.setFixedSize(40, 40)
+                button.setFont(QFont("Arial", 20))
                 button.setStyleSheet("""
                     QPushButton {
                         background-color: #ffffff;
@@ -45,7 +53,8 @@ class MainWindow(QMainWindow):
                 """)
                #  button.clicked.connect(lambda checked, e=emoji: self.emoji_clicked(e))
                 self.emoji_layout.addWidget(button, row_idx, col_idx)
-                self.emoji_buttons.append(button)        
+                self.emoji_buttons.append(button)   
+       
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     window = MainWindow()
