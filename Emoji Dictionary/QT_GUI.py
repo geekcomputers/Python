b6335cd5a0601ace1a5276e8ab1186ced9c2a71c
Pratyanj <pratyanjmodh1205@gmail.com>
2025-06-07 11:44:38 +0530

    change indention and add search_emoji

--- Emoji Dictionary/QT_GUI.py
+++ Emoji Dictionary/QT_GUI.py
@@ -9,52 +9,60 @@ from PyQt5 import uic
 
 class MainWindow(QMainWindow):
    def __init__(self):
-        super(MainWindow, self).__init__()
+      super(MainWindow, self).__init__()
         
-        # Load the UI file
-        uic.loadUi('Emoji Dictionary/QT_GUI.ui', self)
-        self.pushButton_4.clicked.connect(self.close)
-        cells = [
-            
-            ["🐒", "🐕", "🐎", "🐪", "🐁", "🐘", "🦘", "🦈", "🐓", "🐝", "👀", "🦴", "👩🏿", "‍🤝", "🧑", "🏾", "👱🏽", "‍♀", "🎞", "🎨", "⚽"],
-            ["🍕", "🍗", "🍜", "☕", "🍴", "🍉", "🍓", "🌴", "🌵", "🛺", "🚲", "🛴", "🚉", "🚀", "✈", "🛰", "🚦", "🏳", "‍🌈", "🌎", "🧭"],
-            ["🔥", "❄", "🌟", "🌞", "🌛", "🌝", "🌧", "🧺", "🧷", "🪒", "⛲", "🗼", "🕌", "👁", "‍🗨", "💬", "™", "💯", "🔕", "💥", "❤"],
-            ["😀", "🥰", "😴", "🤓", "🤮", "🤬", "😨", "🤑", "😫", "😎"],
-        ]
-        def emoji_wight_btn():
+      # Load the UI file
+      uic.loadUi('Emoji Dictionary/QT_GUI.ui', self)
+      self.pushButton_4.clicked.connect(self.close)
+      cells = [
+         
+         ["🐒", "🐕", "🐎", "🐪", "🐁", "🐘", "🦘", "🦈", "🐓", "🐝", "👀", "🦴", "👩🏿", "‍🤝", "🧑", "🏾", "👱🏽", "‍♀", "🎞", "🎨", "⚽"],
+         ["🍕", "🍗", "🍜", "☕", "🍴", "🍉", "🍓", "🌴", "🌵", "🛺", "🚲", "🛴", "🚉", "🚀", "✈", "🛰", "🚦", "🏳", "‍🌈", "🌎", "🧭"],
+         ["🔥", "❄", "🌟", "🌞", "🌛", "🌝", "🌧", "🧺", "🧷", "🪒", "⛲", "🗼", "🕌", "👁", "‍🗨", "💬", "™", "💯", "🔕", "💥", "❤"],
+         ["😀", "🥰", "😴", "🤓", "🤮", "🤬", "😨", "🤑", "😫", "😎"],
+      ]
+      def emoji_wight_btn():
          if self.emoji_widget.isVisible():
             self.emoji_widget.hide()
          else:
             self.emoji_widget.show() 
+         
+      def search_emoji():
+            word = self.lineEdit.text()            
+            if word == "":
+               self.textEdit.setText("You have entered no emoji.")
+            else:
+               means = emoji.demojize(word)
+               self.textEdit.setText("Meaning of Emoji  :  " + str(word) + "\n\n" + means)
             
-        self.emoji_buttons = []
-        self.emoji_layout = QGridLayout()
-        self.emoji_widget = QWidget()
-        self.emoji_widget.setLayout(self.emoji_layout)
-        self.frame_2.setLayout(QVBoxLayout())
-        self.frame_2.layout().addWidget(self.emoji_widget)
-        self.emoji_widget.hide()
-        self.pushButton.clicked.connect(lambda:emoji_wight_btn())
-        
-        
-        for row_idx, row in enumerate(cells):
-            for col_idx, emoji in enumerate(row):
-                button = QPushButton(emoji)
-                button.setFixedSize(40, 40)
-                button.setFont(QFont("Arial", 20))
-                button.setStyleSheet("""
-                    QPushButton {
-                        background-color: #ffffff;
-                        border: 1px solid #e0e0e0;
-                        border-radius: 5px;
-                    }
-                    QPushButton:hover {
-                        background-color: #f0f0f0;
-                    }
-                """)
-               #  button.clicked.connect(lambda checked, e=emoji: self.emoji_clicked(e))
-                self.emoji_layout.addWidget(button, row_idx, col_idx)
-                self.emoji_buttons.append(button)   
+      self.emoji_buttons = []
+      self.emoji_layout = QGridLayout()
+      self.emoji_widget = QWidget()
+      self.emoji_widget.setLayout(self.emoji_layout)
+      self.frame_2.setLayout(QVBoxLayout())
+      self.frame_2.layout().addWidget(self.emoji_widget)
+      self.emoji_widget.hide()
+      self.pushButton.clicked.connect(lambda:emoji_wight_btn())
+      
+      
+      for row_idx, row in enumerate(cells):
+         for col_idx, emoji in enumerate(row):
+               button = QPushButton(emoji)
+               button.setFixedSize(40, 40)
+               button.setFont(QFont("Arial", 20))
+               button.setStyleSheet("""
+                  QPushButton {
+                     background-color: #ffffff;
+                     border: 1px solid #e0e0e0;
+                     border-radius: 5px;
+                  }
+                  QPushButton:hover {
+                     background-color: #f0f0f0;
+                  }
+               """)
+            #  button.clicked.connect(lambda checked, e=emoji: self.emoji_clicked(e))
+               self.emoji_layout.addWidget(button, row_idx, col_idx)
+               self.emoji_buttons.append(button)   
        
 if __name__ == '__main__':
     app = QApplication(sys.argv)
