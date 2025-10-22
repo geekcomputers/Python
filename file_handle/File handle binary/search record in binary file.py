ca487a07af9385b072c6023a73e42e74265744a3
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2025-10-22 18:45:31 +0530

    snake case folder name

---
+++ file_handle/File handle binary/search record in binary file.py
@@ -0,0 +1,22 @@
+# binary file to search a given record
+
+import pickle
+from dotenv import load_dotenv
+
+
+def search():
+    with open("student_records.pkl", "rb") as F:
+        # your file path will be different
+        search = True
+        rno = int(input("Enter the roll number of the student"))
+
+        for i in pickle.load(F):
+            if i[0] == rno:
+                print(f"Record found successfully\n{i}")
+                search = False
+
+        if search:
+            print("Sorry! record not found")
+
+
+search()
