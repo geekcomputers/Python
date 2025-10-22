95a99576380222b41d2b84873c5a76a7c1c8b737
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2025-10-22 18:57:12 +0530

    fix: count upper and lower accurate

--- file_handle/File handle text/counter.py
+++ file_handle/File handle text/counter.py
@@ -5,30 +5,30 @@ Class resposible for counting words for different files:
 - Code readability
 """
 
+## ! Is there any other way than doing it linear?
 
+
+## ! What will be test cases of it?
+# ! Please do let me know.
 class Counter:
     def __init__(self, text: str) -> None:
         self.text = text
-
         # Define the initial count of the lower and upper case.
         self.count_lower = 0
         self.count_upper = 0
-        self.count()
+        self.compute()
 
-    def count(self) -> None:
+    def compute(self) -> None:
         for char in self.text:
-            if char.lower():
+            if char.islower():
                 self.count_lower += 1
-            elif char.upper():
+            elif char.isupper():
                 self.count_upper += 1
-
-        return (self.count_lower, self.count_upper)
-
     def get_total_lower(self) -> int:
         return self.count_lower
 
     def get_total_upper(self) -> int:
         return self.count_upper
 
-    def get_total(self) -> int:
+    def get_total_chars(self) -> int:
         return self.count_lower + self.count_upper
