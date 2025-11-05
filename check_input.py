5cb7e5124ef555941580d536fd25a9b10fe1b754
khanhkhanhlele <namkhanh2172@gmail.com>
2025-11-05 10:33:38 +0700

    Fix typos in check_input.py

--- check_input.py
+++ check_input.py
@@ -36,5 +36,5 @@ def get_user_input(start, end):
 x = get_user_input(1, 6)
 print(x)
 # Asks user to enter something, ie. a number option from a menu.
-# While type != interger, and not in the given range,
+# While type != integer, and not in the given range,
 # Program gives error message and asks for new input.
