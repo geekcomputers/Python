66867a50bf44ace06b3455b397819a5328253e16
mr-d-luffy <zoroxpro@proton.me>
2025-11-29 00:05:30 +0530

    exception handling of Sockets with Fore color using colorama module

--- Web Socket.py
+++ Web Socket.py
@@ -1,9 +1,16 @@
 # Program to print a data & it's Metadata of online uploaded file using "socket".
 import socket
-skt_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-skt_c.connect(("data.pr4e.org", 80))
-link = "GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n".encode()
-skt_c.send(link)
+from colorama import Fore # this module for Color the font 
+
+# handling the exceptions
+try:
+    skt_c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    skt_c.connect(("data.pr4e.org", 80))
+    link = "GET http://data.pr4e.org/intro-short.txt HTTP/1.0\r\n\r\n".encode()
+    skt_c.send(link)
+except(Exception) as e:
+    # this code runes on error in any connection 
+    print(Fore.RED, e, Fore.RESET)
 
 while True:
     data = skt_c.recv(512)
