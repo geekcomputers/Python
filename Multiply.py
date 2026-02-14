c9ff050d0707ae47240f23854574da80f6ba7e04
Derderderr <bbb8b8bb@gmail.com>
2026-02-14 12:37:11 +0800

    Update Multiply.py

--- Multiply.py
+++ Multiply.py
@@ -1,4 +1,7 @@
 def product(a, b):
+    if b < 0:
+        return -product(a, -b)
+    
     if a < b:
         return product(b, a)
     elif b != 0:
@@ -9,4 +12,4 @@ def product(a, b):
 
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
-print("Product is: ", product(a, b))
+print("Product is:", product(a, b))
